257089e8d623378c4ae54507c07b8daf20e982fc
habi <email@davidhaberthuer.ch>
2013-02-07 14:20:30 +0100

    Succeeded in setting the pins GPIO.py now correctly sets the pins, i.e. alternates between high and low for a set amount and set time.

--- Dev/Elphel/GPIO.py
+++ Dev/Elphel/GPIO.py
@@ -12,23 +12,22 @@ def is_even(i):
 	return (i % 2) == 0
 
 # to use Raspberry Pi board pin numbers
-print 'use Raspberry Pi board pin numbers'
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD) # Named sequentially, as seen on the connector. compare http://elinux.org/File:GPIOs.png
+# GPIO.setmode(GPIO.BCM) # Named GPIO*, see table http://is.gd/xWDsp7 (e.g. 007 is the last pin)
 
-# set up GPIO output channel
 print 'set up GPIO output channel'
-Pin = 11
+Pin = 26
 GPIO.setup(Pin, GPIO.OUT)
 
 # set RPi board pin selected above to high for a certain time, wait, set it low
 # lather, rinse, repeat
-sleepytime = 1
+sleepytime = 0.1
 for Iteration in range(10):
 	if is_even(Iteration):
-		print 'setting high'
+		print 'setting high for',sleepytime,'s'
 		GPIO.output(Pin, GPIO.HIGH)
 		time.sleep(sleepytime)
 	else:
-		print 'setting low'
+		print 'setting low for',sleepytime,'s'
 		GPIO.output(Pin, GPIO.LOW)
 		time.sleep(sleepytime)
