1a9b3d248a4aa63747a669b5bee20dea956f8319
habi <email@davidhaberthuer.ch>
2013-02-07 14:39:21 +0100

    GPIO.py Also reset the Pins according to the last line of the example given on http://code.google.com/p/raspberry-gpio-python/

--- Dev/Elphel/GPIO.py
+++ Dev/Elphel/GPIO.py
@@ -16,14 +16,14 @@ GPIO.setmode(GPIO.BOARD) # Named sequentially, as seen on the connector. compare
 # GPIO.setmode(GPIO.BCM) # Named GPIO*, see table http://is.gd/xWDsp7 (e.g. 007 is the last pin)
 
 print 'set up GPIO output channel'
-Pin = 3 # BOARD
+Pin = 26 # BOARD
 #~ Pin = 007 # BMC
 GPIO.setup(Pin, GPIO.OUT)
 
 # set RPi board pin selected above to high for a certain time, wait, set it low
 # lather, rinse, repeat for 'steps' steps
 sleepytime = 0.5
-steps = 20
+steps = 5
 for Iteration in range(20):
 	if is_even(Iteration):
 		print str(Iteration +1) + '/' + str(steps),'| Pin',Pin,'high for',sleepytime,'s'
@@ -33,3 +33,6 @@ for Iteration in range(20):
 		print str(Iteration +1) + '/' + str(steps),'| Pin',Pin,'low for',sleepytime,'s'
 		GPIO.output(Pin, GPIO.LOW)
 		time.sleep(sleepytime)
+
+# Reset every channel that has been set up by this program to INPUT with no pullup/pulldown and no event detection.
+GPIO.cleanup()
