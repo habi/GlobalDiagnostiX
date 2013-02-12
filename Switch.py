562867d1ce13fd0bacad716a3878f41693824b3a
habi <email@davidhaberthuer.ch>
2013-02-12 20:05:16 +0100

    Switch.py now fully conforms to PEP 8, at least Geany tells me it "compiles" without errors.

--- Switch.py
+++ Switch.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-"""
+'''
 Script to switch Network interfaces between DHCP and Elphel
 First version based on reading the values from their respective outputs
 New version simply sets to Elphel as default when called without a flag.
 Options -p or -p switch to the *P*SI *D*HCP server
-"""
+'''
 
 import os
 from optparse import OptionParser
@@ -15,16 +15,17 @@ Parser = OptionParser()
 Usage = 'Usage: % prog [Options] arg'
 
 Parser.add_option('-e', '--Elphel', dest='Elphel',
-                  help=('Switch the network interface to work with the Elphel '
-                  'camera (default)'),
+                  help='(Switch the network interface to work with the Elphel '
+                  'camera (default)',
                   default=True,
                   action='store_true')
 Parser.add_option('-d', '--DHCP', dest='DHCP',
-                  help=('Switch the network interface to *D*HCP, to work '
-                  'with PSI'),
+                  help='Switch the network interface to *D*HCP, to work '
+                  'with PSI ethernet',
                   action='store_true')
 Parser.add_option('-p', '--PSI', dest='PSI',
-                  help='Switch the network interface to DHCP, to work with *P*SI',
+                  help='Switch the network interface to DHCP, to work with '
+                  '*P*SI ethernet',
                   action='store_true')
 (Options, Arguments) = Parser.parse_args()
 
@@ -40,13 +41,13 @@ elif Options.Elphel:
            '"sudo ifconfig eth0 192.168.0.1 > /dev/null"')
     os.system('sudo ifconfig eth0 192.168.0.1 > /dev/null')
     print
-    print ('Uplug the PSI Ethernet cable, plug in the Elphel cable and',
+    print ('Uplug the PSI Ethernet cable, plug in the Elphel cable and '
            'wait a while for the camera to boot')
 
-if Options.DHCP or Options.PSI:
+if not Options.Elphel:
     print 'If you just switched from Elphel to to PSI,'
     print ' then it is probably a good idea to run'
     print 'cd;bash SyncToAFS.cmd'
-    print ('to rsync all changes in the home directory to'
+    print ('to rsync all changes in the home directory to '
            '/afs/EssentialMed/Dev/RPI')
-    print 'and maybe also do a "git commit -a;git push"...'
+    print 'and maybe also do a "git commit -a" and a "git push"'
