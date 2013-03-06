f570e514df43bd48ebf31f8d03ec61a94c800aea
David Haberthür <email@davidhaberthuer.ch>
2013-03-06 14:11:16 +0100

    Triggeredexposure.py - Set up pin outs correctly - Try to think of a way of doing the spinup and other timing correctly. Hard to do withouth the actual setup... - Prettyfied some output

--- Dev/Elphel/TriggeredExposure.py
+++ Dev/Elphel/TriggeredExposure.py
@@ -64,21 +64,19 @@ except:
 
 """
 The script will do this:
-- Set exposure time, either via wget or PHP call to something along the lines
-    of snapfull.php
-- Spin up anode with a 4V signal over one pin
-- Wait for camera signaling to be ready
+- Set exposure time of the camera
+- Spin up anode with a 3.2V signal over one pin (hope that's enough, since
+    we measured 4.2V over the trigger in the x-ray lab)
+- Wait for camera signaling to be ready with an exposure
 - Trigger camera exposure, marginally longer than x-ray pulse, so we catch all
     photons
-- Trigger x-ray pulse with another 4V signal over another pin
-- Wait for afterglow (exact time TBD) to catch all photons
-- Get image from camera
-- Save it someplace sensible
+- Trigger x-ray pulse with another 3.2V signal over another pin
+- Download image from from camera and save it someplace sensible
 - Process the Image, or show it
 """
 
 # http://wiki.elphel.com/index.php?title=Imgsrv#imgsrv_usage describes what
-# the StartURL and ImageURL commands to. They do some pointery thingies.
+# the StartURL and ImageURL commands do; some pointery thingies.
 CamIP = 'http://192.168.0.9'
 StartURL = CamIP + ':8081/towp/save'
 ImageURL = CamIP + ':8081/torp/wait/img/next/save'
@@ -94,14 +92,13 @@ CameraCommand += '&AUTOEXP_ON=0'  # turn off auto-exposure
 # set exposure time to 'Exposure time in microseconds'.
 # We input it in ms, so we multiply by 1000
 CameraCommand += '&EXPOS=' + str(options.ExposureTime * 1000)
-CameraCommand += '&WB_EN=0'  # turn off white balance
 # reset sensor to full size ("Sensor width is 'reduced' to full sensor
-# if set to 10000")
+# if set to 10000"). Just make sure we get the full image
 CameraCommand += '&WOI_WIDTH=10000&WOI_HEIGHT=10000'
 # set camera to be UN-triggered, i.e. internally triggered
 CameraCommand += '&TRIG=0"'
 #~ CameraCommand += '&TRIG=4"'  # set camera to be triggered externally
-# delete wget output afterwards
+# get rid of the 'wget' output after it is finished.
 CameraCommand += ' --delete-after'
 if options.Test:
     print 'TESTING: I would set up the camera with'
@@ -110,15 +107,9 @@ if options.Test:
     print '---'
 else:
     os.system(CameraCommand)
-# Spin up anode with a 4V signal over one pin
-# Set up RPi pins as they are on the board (pysical location). Check
-# http://elinux.org/File:GPIOs.png to see what each pin does or which
-# number it actually is. Pins 2 and 4 output 5V, Pin 6 is Ground.
-if options.Test is False:
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(AnodePin, GPIO.OUT)
+
 # Inform the user what will happen
-print 'As soon as you confirm with [Enter], I will trigger everything:'
+print 'As soon as you confirm with [Enter], I will trigger everything like so:'
 print '    - I will spin up the anode for', AnodeSpinupTime, 'seconds'
 print '    - Meanwhile I will prepare the camera'
 print '    - I will trigger the camera to acquire an image with an exposure', \
@@ -127,23 +118,32 @@ print '    - I will trigger the camera to acquire an image with an exposure', \
     round(float(int(options.ExposureTime * CameraExposureFactor)) / 1000, 3), \
     's,', CameraExposureFactor, 'x longer than the set exposure time)'
 print '    - I will trigger the x-ray pulse for', options.ExposureTime, 'ms'
+print '    - I will save the image to ', os.path.join(os.getcwd(), SubDirName)
 
 # Go!
 raw_input('Simulate a trigger by pressing Enter... [Enter]')
 print
 
-# Set the AnodePin to high to spin up the anode
-if options.Test:
-    print 'TESTING: Spinning up anode'
-else:
-    GPIO.output(AnodePin, GPIO.HIGH)
 # Prepare the camera
 # First set the pointer
 if options.Test:
     print 'TESTING: Calling', StartURL, 'to set the camera pointers'
 else:
     urllib.urlopen(StartURL)
-# Wait for camera signaling that it is ready by listening on the input port...
+# Set up RPi pins as they are on the board (pysical location). Check
+# http://elinux.org/File:GPIOs.png to see what each pin does or which
+# number it actually is.
+if options.Test:
+    print 'TESTING: Setting up board pins layout and "AnodePin"'
+        print 'TESTING: Spinning up anode'
+else:
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(AnodePin, GPIO.OUT)
+    # Set the 'AnodePin' to HIGH to spin up the anode
+    GPIO.output(AnodePin, GPIO.HIGH)
+    # wait for half a second until the anode is ready
+    time.slee(0.5)
+# Wait for camera signaling that it is ready by listening on the input port
 if options.Test:
     print 'TESTING: Setting up "InputPin"'
 else:
@@ -154,7 +154,7 @@ if options.Test:
     print 'TESTING: Sleeping for "options.ExposureTime"'
 else:
     while True:
-        # Wait for camera to signal readyness on the InputPin
+        # Wait for camera to signal readyness on the InputPin, then go!
         if (GPIO.input(InputPin)):
             # Trigger x-ray pulse with another 4V signal over another pin
             GPIO.output(XraySourcePin, GPIO.HIGH)
@@ -162,8 +162,10 @@ else:
             print 'Do something sensible here'
 # Wait for afterglow (exact time TBD) to catch all photons
 time.sleep(0.01)
-# Get image from camera and save it
-ImageName = time.time()  # current epoch time as filename, avoiding duplicates
+
+# Get image from camera and save it with current epoch time as filename, to
+# avoid duplicates
+ImageName = time.time()
 if options.Test:
     print 'TESTING: I would save', ImageURL, 'to', \
         os.path.join(SubDirName, str(ImageName) + '.jpg')
@@ -178,7 +180,7 @@ else:
     GPIO.cleanup()
 
 print
-# 38 = 80 / 2 - len('Done') / 2, 80 being the line length
+# 38 = 80 / 2 - len('Done') / 2, with 80 being the line length
 print 38 * ' ' + 'DONE!'
 print
 # Process the Image, and/or show it afterwards.
