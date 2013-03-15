265eca4b5ade953583b32507d043953aa5666322
David Haberthür <email@davidhaberthuer.ch>
2013-03-15 17:47:42 +0100

    Trying to get the hang of PHP variable calling

--- Dev/Elphel/globaldiagnostix.php
+++ Dev/Elphel/globaldiagnostix.php
@@ -5,6 +5,7 @@ FTP this file to ftp://CameraIP/var/html and look at it at
 http://CameraIP/var/globaldiagnostix.php
 */
 
+/*
 echo "Hallihallo<br/>";
 
 echo "Original state was=".elphel_get_state()."<br/>\n";
@@ -12,7 +13,8 @@ if (elphel_get_state()>7) {  //! compressor is running
 	echo "1<br/>";
 	elphel_compressor_stop(); //! stop it
 	echo "2<br/>";
-	//! while (elphel_get_state()>7) usleep (100000) ; //! just wait - will wait forever if async mode
+	// while (elphel_get_state()>7) usleep (100) ; //! just wait - will wait forever if async mode
+	// currently, the cam is in async mode, if you uncomment the line above, nothing happens
 	echo "3<br/>";
 	elphel_compressor_reset(); //! Maybe needed twice
 	echo "4<br/>";
@@ -21,36 +23,40 @@ if (elphel_get_state()>7) {  //! compressor is running
 echo "Before sensor reset state was=".elphel_get_state()."<br/>\n";
 
 echo "<br/>Hallihallo<br/><br/>";
+*/
 
 echo "Image before doing anything<br/>";
 echo "<a href='http://192.168.0.9:8081/img'>
 		<img src='http://192.168.0.9:8081/img'
 			width='200'
 			alt='Image before changing settings'>
-	</a>";
-
-echo "<pre>\n";
+	</a><br/>";
 
 $init_pars=array(
-	"DCM_HOR"  => 1,  //! Decimation horizontal
-	"DCM_VERT" => 1,  //! Decimation vertical
-	"BIN_HOR"  => 10,  //! Binning horizontal
-	"BIN_VERT" => 10,  //! Binning vertical
-	"QUALITY"  => 90, //! JPEG quality (%)
-	"BITS" => 8,                     //! 8-bit image mode (may be 16)
-	"GAMMA" => 57,                   //! Gamma=57%
-	"PIXEL_LOW" => 10,               //! Black level - 10 (sensor default "fat zero")
-	"PIXEL_HIGH" => 254,             //! white level
-	"EXPOS" =>    500,               //! whatever? (in 100usec steps)
-	"WOI_LEFT" =>  0,                 //! window left
-	"WOI_TOP" =>   0,                 //! window top
-	"WOI_WIDTH" =>  10000,            //! window width  (more than needed, will be truncated)
-	"WOI_HEIGHT" => 10000,            //! window height (more than needed, will be truncated)
-	"BAYER" =>          4             //! 0..3 - set, 4- use calcualted
+	"DCM_HOR"  => 1,	//! Decimation horizontal
+	"DCM_VERT" => 1,	//! Decimation vertical
+	"BIN_HOR"  => 10,	//! Binning horizontal
+	"BIN_VERT" => 10,	//! Binning vertical
+	"QUALITY"  => 90,	//! JPEG quality (%)
+	"BITS" => 8,		//! 8-bit image mode (may be 16)
+	"GAMMA" => 57,		//! Gamma=57%
+	"PIXEL_LOW" => 10,	//! Black level - 10 (sensor default "fat zero")
+	"PIXEL_HIGH" => 254,	//! white level
+	"EXPOS" =>    500,	//! whatever? (in 100usec steps)
+	"WOI_LEFT" =>  0,	//! window left
+	"WOI_TOP" =>   0,	//! window top
+	"WOI_WIDTH" =>  10000,	//! window width  (more than needed, will be truncated)
+	"WOI_HEIGHT" => 10000,	//! window height (more than needed, will be truncated)
+	"BAYER" =>          4	//! 0..3 - set, 4- use calculated
 	);
 
-echo "Parameters before programmimg:\n";
+echo "The current exposure of the camera is ".elphel_get_P_value(ELPHEL_EXPOS)." usec<br>";
+echo "We would like to set the exposure time to ".$init_pars["EXPOS"]." usec<br>";
+
+echo "<br>Parameters before programming:\n";
+echo "<pre>\n";
 print_r(elphel_get_P_arr($init_pars));
+echo "</pre>\n";
 
 $phase_pars=array();
 if ($_GET["phase"]) $phase_pars["SENSOR_PHASE"]=$_GET["phase"]+0;
@@ -61,8 +67,8 @@ if (($_GET["phase"]) && ($_GET["clk"]==null)) { /// change phase, use default cl
 	printf ("Writing %d parameters to control sensor phase to the camera before reset\n",elphel_set_P_arr($phase_pars));
 	print_r($phase_pars);
 }
-//!  elphel_reset_sensor();
-//!  echo "After reset & initialization: elphel_get_state=".elphel_get_state()."<br/>\n";
+elphel_reset_sensor();
+echo "After reset & initialization: elphel_get_state=".elphel_get_state()."<br/>\n";
 printf ("Written %d parameters to the camera\n",elphel_set_P_arr($init_pars));
 //! Program sensor (with restart)
 if ($_GET["clk"]) { /// modify both clock and phase
@@ -71,13 +77,14 @@ if ($_GET["clk"]) { /// modify both clock and phase
 }
 echo "After programming sensor parameters: elphel_get_state=".elphel_get_state()."<br/>\n";
 echo "Parameters after programmimg:\n";
+echo "<pre>\n";
 print_r(elphel_get_P_arr($init_pars));
 if (count($phase_pars)) print_r(elphel_get_P_arr($phase_pars));
 elphel_compressor_run();
 echo "After starting compressor - elphel_get_state=".elphel_get_state()."<br/>\n";
 echo "</pre>\n";
 
-echo "Image with new settings<br/>";
+echo "Image with 'new' settings<br/>";
 echo "<a href='http://192.168.0.9:8081/img'>
 		<img src='http://192.168.0.9:8081/img'
 			width='200'
