4d70d10411bc0967c91616e8cc8d32d6522690cd
David Haberthür <email@davidhaberthuer.ch>
2013-03-15 17:12:48 +0100

    copy all PHP files to the camera

--- Dev/Elphel/ftp_files_to_camera.sh
+++ Dev/Elphel/ftp_files_to_camera.sh
@@ -1,2 +1,4 @@
+curl -T globaldiagnostix.php ftp://192.168.0.9/var/html/ --user root:pass
 curl -T camera_demo.php ftp://192.168.0.9/var/html/ --user root:pass
+curl -T snapfull.php ftp://192.168.0.9/var/html/ --user root:pass
 
