e3cb931eee5cbd68cd27616e2f410ebac262bad4
David Haberthür <email@davidhaberthuer.ch>
2013-03-19 13:33:13 +0100

    FTP some more files to the camera

--- Dev/Elphel/ftp_files_to_camera.sh
+++ Dev/Elphel/ftp_files_to_camera.sh
@@ -1,4 +1,4 @@
+# curl -T camera_demo.php ftp://192.168.0.9/var/html/ --user root:pass
 curl -T globaldiagnostix.php ftp://192.168.0.9/var/html/ --user root:pass
-curl -T camera_demo.php ftp://192.168.0.9/var/html/ --user root:pass
+curl -T setparameters_demo.php ftp://192.168.0.9/var/html/ --user root:pass
 curl -T snapfull.php ftp://192.168.0.9/var/html/ --user root:pass
-
