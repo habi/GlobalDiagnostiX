7a56b1136e50465e1250663aa8e53b3bd5593c57
David Haberthür <email@davidhaberthuer.ch>
2013-03-25 17:23:46 +0100

    FTP the files a bit more cleverly

--- Dev/Elphel/ftp_files_to_camera.sh
+++ Dev/Elphel/ftp_files_to_camera.sh
@@ -1,4 +1,5 @@
-# curl -T camera_demo.php ftp://192.168.0.9/var/html/ --user root:pass
-curl -T globaldiagnostix.php ftp://192.168.0.9/var/html/ --user root:pass
-curl -T setparameters_demo.php ftp://192.168.0.9/var/html/ --user root:pass
-curl -T snapfull.php ftp://192.168.0.9/var/html/ --user root:pass
+# look for all PHP files in current directory
+# for each of those, generate an FTP command to transfer them to the camera
+for i in `ls *.php`
+do curl -T $i ftp://192.168.0.9/var/html/ --user root:pass
+done
