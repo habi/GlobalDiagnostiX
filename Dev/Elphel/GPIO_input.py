08d3b716278bfa6d946795b024d17352a0166a9a
David Haberthür <email@davidhaberthuer.ch>
2013-03-26 17:07:23 +0100

    GPIO
    Renamed GPIO.py to GPIO_output.py. Tiny script GPIO_input reads input on a desired pin. This works fine with the triggered output signal of the camera (which would go to the blue LED on the cable). We can detect triggers from the camera on the Pi, jupi!

---
+++ Dev/Elphel/GPIO_input.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# coding=utf8
+
+# Script to work with the Input/Output Pins of the RPi
+# Ultimately thought to trigger the Elphel camera
+# Based on http://code.google.com/p/raspberry-gpio-python/
+
+import sys
+import time
+# Try to import the GPIO library
+try:
+	import RPi.GPIO as GPIO
+except:
+	print 'I cannot import RPI.GPIO, you have to run the script as root'
+	print 'try running it again with'
+	print '---'
+	print 'sudo',' '.join(sys.argv) # joining the sys.argv list to a string so we can print it
+	print '---'
+	sys.exit(1)
+
+def is_even(i):
+	return (i % 2) == 0
+
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD) # Named sequentially, as seen on the connector. compare http://elinux.org/File:GPIOs.png
+# GPIO.setmode(GPIO.BCM) # Named GPIO*, see table http://is.gd/xWDsp7 (e.g. 007 is the last pin)
+
+print 'set up GPIO input channel'
+Pin = 26 # BOARD
+#~ Pin = 007 # BMC
+GPIO.setup(Pin, GPIO.IN)
+
+print
+print 'I am waiting for you to connect pin', Pin, 'and ground'
+print
+
+# Wait for Input, then print something and wait for a short while
+# Code according to http://is.gd/G88UyN
+counter = 1
+Previous_Reading = 0
+while True:
+	if GPIO.input(Pin):
+		print "Pin", Pin, "and Ground are connected (" + str(counter),\
+			  "times)."
+		counter += 1
+		time.sleep(0.05)
+
+#~ Counter = 1
+#~ Previous_Input = 0
+#~ while Counter < 100:
+	#~ Input = GPIO.input(Pin)
+	#~ if not Previous_Input and Input:
+		#~ print "Pin", Pin, "and Ground are connected (" + str(Counter),\
+			  #~ "times)."
+		#~ Counter += 1
+	#~ Previous_Input = Input
+	#~ time.sleep(0.1)
+
+# Reset every channel that has been set up by this program to INPUT with no pullup/pulldown and no event detection.
+GPIO.cleanup()
