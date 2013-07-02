6d32d4bd59c206fa223ec04f3148168ef6915927
David Haberthür <email@davidhaberthuer.ch>
2013-07-02 16:20:31 +0200

    MTF_reader_and_plotter.py updated
    - Some more options at the beginning
    - User can either select coordinates for edge-plotting or use predefined ones
    - User can either select biggest step or let the script calculate the knife-edge from  the (firs) derivative
    - Acutally plotting LSF and MTF

--- MTF_reader_and_plotter.py
+++ MTF_reader_and_plotter.py
@@ -10,6 +10,19 @@ import os
 
 ion()
 
+# SETUP
+SelectStartPointManually = False
+SelectEdgeManually = False
+EdgeRange = 100
+
+# Images
+ImagePath = '/afs/psi.ch/project/EssentialMed/Images'
+ImageDir = '11-MTF'
+#~ ImageFile = 'iPhone_with_xray_film.jpg'
+#~ ImageFile = 'iPhone_with_xray_film_hdr.jpg'
+#~ ImageFile = 'iPhone_with_xray_film_window.jpg'
+ImageFile = 'iPhone_with_xray_film_window_hdr.jpg'
+
 def MTF(edgespreadfunction):
     '''
     Compute the modulation transfer function (MTF).
@@ -21,7 +34,7 @@ def MTF(edgespreadfunction):
     '''
     linespreadfunction = np.diff(edgespreadfunction)
     return np.abs(np.fft.fft(linespreadfunction))
-    
+
 
 def LSF(edgespreadfunction):
     '''
@@ -40,16 +53,9 @@ def rgb2gray(rgb):
     convert an image from rgb to grayscale
     http://stackoverflow.com/a/12201744/323100
     '''
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])    
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.144])
 
-ImagePath = '/afs/psi.ch/project/EssentialMed/Images'
-ImageDir = '11-MTF'
-ImageFile = 'iPhone_with_xray_film.jpg'
-ImageFile = 'iPhone_with_xray_film_hdr.jpg'
-ImageFile = 'iPhone_with_xray_film_window.jpg'
-ImageFile = 'iPhone_with_xray_film_window_hdr.jpg'
-
-ImageToLoad = os.path.join(ImagePath,ImageDir,ImageFile)
+ImageToLoad = os.path.join(ImagePath, ImageDir, ImageFile)
 
 # Read the image and convert it to grayscale rightaway
 ImageRGB = plt.imread(ImageToLoad)
@@ -57,12 +63,17 @@ Image = rgb2gray(ImageRGB)
 
 ImageWidth = Image.shape[0]
 ImageHeight = Image.shape[1]
-print 'The image we loaded is', ImageWidth, 'by', ImageHeight,\
-    'pixels big. That is', round(ImageWidth * ImageHeight/1e6,3), 'MPx.'
+print 'The image we loaded is', ImageWidth, 'by', ImageHeight, \
+    'pixels big. That is', round(ImageWidth * ImageHeight/1e6, 3), 'MPx.'
 
 plt.subplot(221)
-plt.imshow(Image,origin='lower',cmap=plt.gray())
-PickPoint = ginput(1)
+plt.imshow(ImageRGB, origin='lower')
+plt.title('Pick point for drawing\n horizontal and vertical profile')
+if SelectStartPointManually:
+    PickPoint = ginput(1)
+else:
+    PickPoint = [[1400,1400]]
+plt.title('Original image')
 Horizon = int(PickPoint[0][1])
 Vertigo = int(PickPoint[0][0])
 print 'selected horizontal line', Horizon, 'and vertical line', Vertigo
@@ -70,62 +81,70 @@ plt.hlines(Horizon, 0, ImageHeight, 'r')
 plt.vlines(Vertigo, 0, ImageWidth, 'b')
 plt.draw()
 plt.subplot(223)
-HorizontalProfile = Image[Horizon,:]
-plt.plot(HorizontalProfile,'r')
+HorizontalProfile = Image[Horizon, :]
+plt.plot(HorizontalProfile, 'r')
 plt.title('Horizontal Profile')
-plt.xlim(0,ImageHeight)
-plt.ylim(0,256)
+#~ plt.xlim(0, ImageHeight)
+#~ plt.ylim(0, 256)
 plt.subplot(222)
-VerticalProfile = Image[:,Vertigo]
-plt.plot(VerticalProfile,range(ImageWidth),'b')
-plt.xlim(0,256)
-plt.ylim(0,ImageWidth)
+VerticalProfile = Image[:, Vertigo]
+plt.plot(VerticalProfile, range(ImageWidth), 'b')
+#~ plt.xlim(0, 256)
+#~ plt.ylim(0, ImageWidth)
 plt.title('Vertical Profile')
 plt.draw()
 
-#~ for item in HorizontalProfile:
-    #~ if item > 250:
-        #~ print HorizontalProfile[item], 'is larger than 250'
-        
-print VerticalProfile
-
-Saturated = [int(x) for x in HorizontalProfile if x >= 245]
-
-print max(HorizontalProfile)
-print max(VerticalProfile)
-     
-
-
-
-
-
-
-
-
-
-
-plt.show()
-ioff()
-exit()
+print 'The horizontal profile (red) goes from', min(HorizontalProfile), 'to',\
+    max(HorizontalProfile)
+print 'The vertical profile (blue) goes from', min(VerticalProfile), 'to',\
+    max(VerticalProfile)
 
 plt.figure()
-for i in range(3):
-    plt.subplot(2, 4, i+1)
-    plt.imshow(ImageRGB[:,:,i],origin='lower',cmap=plt.gray())
-    plt.title(' '.join(['Channel', str(i)]))
-    plt.subplot(2, 4, i+1+4)
-    plt.imshow(ImageRGB[:,:,0]-Image[:,:,i],origin='lower',cmap=plt.gray())
-    plt.title(' '.join(['Diff 0 &', str(i)]))
-plt.subplot(244)
-plt.imshow(ImageRGB,origin='lower')
-plt.title('Original')
-plt.subplot(248)
-plt.imshow(rgb2gray(Image),origin='lower')
-plt.title('rgb21gray(Original)')
+plt.subplot(211)
+plt.plot(VerticalProfile)
+if SelectEdgeManually:
+    plt.title('Select approximate middle of knife edge')
+    EdgePosition = ginput(1)
+    plt.title('Vertical Profile')
+else:
+    EdgePosition = [[LSF(VerticalProfile).argmax(),np.nan]]
+    plt.title('Vertical Profile\n(selected automatically)')
+plt.axvspan(EdgePosition[0][0]-EdgeRange, EdgePosition[0][0]+EdgeRange,
+            facecolor='r', alpha=0.5)
+plt.subplot(234)
+plt.plot(range(int(EdgePosition[0][0])-EdgeRange, int(EdgePosition[0][0])+EdgeRange),
+         VerticalProfile[int(EdgePosition[0][0])-EdgeRange:
+                         int(EdgePosition[0][0])+EdgeRange])
+plt.title('Zoomed edge')
+plt.xlim(EdgePosition[0][0]-EdgeRange, EdgePosition[0][0]+EdgeRange)
+plt.subplot(235)
+plt.plot(LSF(VerticalProfile[EdgePosition[0][0]-EdgeRange:
+                             EdgePosition[0][0]+EdgeRange]))
+plt.title('LSF')
+plt.subplot(236)
+plt.plot(MTF(VerticalProfile[EdgePosition[0][0]-EdgeRange:
+                             EdgePosition[0][0]+EdgeRange]))
+plt.title('MTF')
+
+print LSF(VerticalProfile).max(), 
+
+plt.figure()
+plt.subplot(311)
+plt.plot(VerticalProfile)
+plt.axvspan(EdgePosition[0][0]-EdgeRange, EdgePosition[0][0]+EdgeRange,
+            facecolor='r', alpha=0.5)
+plt.subplot(312)
+plt.plot(LSF(VerticalProfile))
+plt.axvspan(EdgePosition[0][0]-EdgeRange, EdgePosition[0][0]+EdgeRange,
+            facecolor='r', alpha=0.5)
+plt.subplot(313)
+plt.plot(MTF(VerticalProfile))
+
+#~ plt.figure()
+#~ plt.text(0.5, 0.5,'matplotlib',
+     #~ horizontalalignment='center',
+     #~ verticalalignment='center')
+#~ plt.text(LSF(VerticalProfile).max(), LSF(VerticalProfile)[LSF(VerticalProfile).argmax()], 'asdf')
+
+ioff()
 plt.show()
-
-#~ plt.imshow(Image,cmap="Greys_r")
-#~ plt.show()
-
-print ImageToLoad
-exit()
