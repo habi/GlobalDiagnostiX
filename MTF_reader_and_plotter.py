960077e7c966f2068abf3a30d5b399541e4f5e61
David Haberthür <email@davidhaberthuer.ch>
2013-07-16 17:50:18 +0200

    Added more cameras to MTF_reader I've put in some minimal logic to switch between the three cases of cameras I'd like to know their MTF of. Depending on the camera I choose a different point for the profile selection (or just the middle). The zoomed region of the edge is now chosen to be (a rounded) 5% of the image width. Some more info in the figure.

--- MTF_reader_and_plotter.py
+++ MTF_reader_and_plotter.py
@@ -11,11 +11,10 @@ import os
 ion()
 
 # SETUP
-SelectStartPointManually = Falseor
+SelectStartPointManually = False
 SelectEdgeManually = False
-EdgeRange = 100
 PolynomialOrder = 5
-RunningOnPi = True
+RunningOnPi = False
 
 # Images
 if RunningOnPi:
@@ -23,10 +22,36 @@ if RunningOnPi:
 else:
     ImagePath = '/afs/psi.ch/project/EssentialMed/Images'
 ImageDir = '11-MTF'
-#~ ImageFile = 'iPhone_with_xray_film.jpg'
-#~ ImageFile = 'iPhone_with_xray_film_hdr.jpg'
-#~ ImageFile = 'iPhone_with_xray_film_window.jpg'
-ImageFile = 'iPhone_with_xray_film_window_hdr.jpg'
+
+#~ Camera = 'iPhone'
+Camera = 'tiscam'
+#~ Camera = 'Elphel'
+
+if Camera == 'iPhone':
+    # use iPhone images
+    ImageFile = 'iPhone_with_xray_film.jpg'
+    ImageFile = 'iPhone_with_xray_film_hdr.jpg'
+    ImageFile = 'iPhone_with_xray_film_window.jpg'
+    ImageFile = 'iPhone_with_xray_film_window_hdr.jpg'
+elif Camera == 'tiscam':
+    # 'The imaging source' camera images from different objectives
+    Objective = 9  # 3,6 or 9
+    if Objective == 3:
+        ObjectiveDir = 3.6
+        ImageFile = 'shot0099.png'  # visually the best one
+    elif Objective == 6:
+        ObjectiveDir = 6.0
+        ImageFile = 'shot0364.png'  # visually the best one
+    elif Objective == 9:
+        ObjectiveDir = 9.6
+        ImageFile = 'shot0072.png'  # visually the best one
+    Camera = Camera + '_' + str(ObjectiveDir)
+elif Camera == 'Elphel':
+    # Elphel images
+    ImageFile = 'image.jpg'
+else:
+    print 'I do not know what to do, exiting'
+    exit()
 
 
 def MTF(edgespreadfunction):
@@ -70,7 +95,8 @@ def polynomialfit(data, order):
     coefficients = np.polyfit(x, y, order)
     return np.polyval(coefficients, x)
 
-ImageToLoad = os.path.join(ImagePath, ImageDir, ImageFile)
+ImageToLoad = os.path.join(ImagePath, ImageDir, Camera, ImageFile)
+print 'reading', ImageToLoad
 
 # Read the image and convert it to grayscale rightaway
 ImageRGB = plt.imread(ImageToLoad)
@@ -87,7 +113,13 @@ plt.title('Pick point for drawing\n horizontal and vertical profile')
 if SelectStartPointManually:
     PickPoint = ginput(1)
 else:
-    PickPoint = [[1500, 1000]]
+    if Camera == 'iPhone':
+        PickPoint = [[1500, 1000]]
+    elif Camera[:6] == 'tiscam':
+        # Select middle of image...
+        PickPoint = [[ImageHeight/2, ImageWidth/2]]
+    elif Camera == 'Elphel':
+        PickPoint = [[ImageHeight/2, ImageWidth/2]]
 plt.title('Original image')
 Horizon = int(PickPoint[0][1])
 Vertigo = int(PickPoint[0][0])
@@ -117,16 +149,22 @@ print 'The horizontal profile (red) goes from', min(HorizontalProfile), 'to',\
 print 'The vertical profile (blue) goes from', min(VerticalProfile), 'to',\
     max(VerticalProfile)
 
+# Set range of the region we want to look at to 'Edgerange', about 10% of Image
+# width
+EdgeRange = int(round(Image.shape[0] * .05/10)*10)
+
 plt.figure(figsize=(16, 9))
 plt.subplot(311)
 plt.plot(VerticalProfile)
 if SelectEdgeManually:
     plt.title('Select approximate middle of knife edge')
     EdgePosition = ginput(1)
-    plt.title('Vertical Profile')
+    plt.title('Vertical Profile\n(zoom reguion selected manually, width = ' +
+              str(EdgeRange) + ' px, approx. 5% of image)')
 else:
     EdgePosition = [[LSF(VerticalProfile).argmax(), np.nan]]
-    plt.title('Vertical Profile\n(selected automatically)')
+    plt.title('Vertical Profile\n(zoom region selected automatically, width ' +
+              '= ' + str(EdgeRange) + ' px, approx. 5% of image)')
 plt.axvspan(EdgePosition[0][0]-EdgeRange, EdgePosition[0][0]+EdgeRange,
             facecolor='r', alpha=0.5)
 
