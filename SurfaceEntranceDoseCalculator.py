fcf6dc203fbc13a2d469482fafc361127de772d3
David Haberthür <email@davidhaberthuer.ch>
2013-10-17 16:56:34 +0200

    Quick and dirty SED calculation
    Taking care that we don't divide by integers, since that f%çked up the
    results a bit.

---
+++ SurfaceEntranceDoseCalculator.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Tiny tool to calculate the surface entrance dose of a certain x-ray measurement
+based on commandline input.
+Gives the same results as the 'Diagnostische Referenzwerte' Excel calculator on
+the BAG-page, in the right side-bar of http://is.gd/E2qIPA.
+The calculation is based on 'Merkblatt R-06-04' from BAG.
+The file is based on SurfaceEntranceDose.py, which was used to plot the SED for
+a talk at an SLS Symposium.
+"""
+
+from __future__ import division  # fix integer division
+from optparse import OptionParser
+import sys
+
+
+# Use Pythons Optionparser to define and read the options, and also
+# give some help to the user
+parser = OptionParser()
+usage = "usage: %prog [options] arg"
+parser.add_option('-v', '--kilovolt', dest='kV',
+                  type='float',
+                  help='set kV',
+                  metavar='90')
+parser.add_option('-a', '--milliamperesecond', dest='mAs',
+                  type='float',
+                  help='set mAs',
+                  metavar='125')
+parser.add_option('-d', '--distance', dest='FocusDistance',
+                  type='float',
+                  default=140.,
+                  help='Focus distance in cm. Defaults to 140 cm',
+                  metavar='120')
+(options, args) = parser.parse_args()
+
+# show the help if no parameters are given
+if options.kV is None or options.mAs is None:
+    parser.print_help()
+    print 'Example:'
+    print 'The command below calculates the surface entrance dose of a',\
+        '"Chest: PA standing", where the WHO manual gives 120 kV and 2 mAs',\
+        'as base values. The distance is shortened fromt the default 140 cm',\
+        'to 1 meter.'
+    print
+    print sys.argv[0], '-v 120 -a 2 -d 100'
+    print
+    sys.exit(1)
+
+# Parameters
+# The K-value is based on the machine. The BAG-calculator (see below) list 0.1
+K = 0.1
+# RSF as found by Arouna2000, cited by BAG2012. *This* RSF gives the same SED
+# values as the XLS-calculator from BAG (http://is.gd/oTpniQ) which I copied to
+# /afs/psi.ch/project/EssentialMed/PresentationsAndInfo/BAG/R-0 DRWCalc 5.0.xls
+RSF = 1.35
+# RSF as found in BAG2012. "Der über verschiedene Anlagen gemittelte
+# Korrekturfaktor betrug 1.15"
+#RSF = 1.15
+
+# calculating while converting Focusdistance from m to cm
+SED = K * (options.kV / 100) ** 2 * options.mAs *\
+    (100. / options.FocusDistance) ** 2 * RSF
+print
+print 'Calculating the surface entrance dose for an x-ray pulse with',\
+    options.kV, 'kV and', options.mAs, 'mAs with a focal distance of',\
+    options.FocusDistance / 100, 'm. The characteristic constant of the',\
+    'x-ray setup is set to', K, 'mGy/mA, the "Rückstreufaktor" RSF was set',\
+    'to', str(RSF) + '. The surface entrance dose is thus SED =',\
+    'K*(U/100)^2*Q*(1/FOD)^2*RSF=' + str(round(SED, 3)), 'mGy'
