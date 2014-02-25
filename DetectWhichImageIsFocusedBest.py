4e6769687df1170fadd8ac8a3fb7214adb8c0619
David Haberthür <email@davidhaberthuer.ch>
2014-02-25 17:56:09 +0100

    Switch from hard-coded directories to what's there
    Reading directories in given base-directory, present those to the user
    and let him/her choose.
    Read png images (if present) and plot the (normalized) exposure and
    (normalized) STD of each image to choose the best focused one.

--- DetectWhichImageIsFocusedBest.py
+++ DetectWhichImageIsFocusedBest.py
@@ -18,43 +18,62 @@ import numpy
 
 # Setup
 Root = '/afs/psi.ch/project/EssentialMed/Images/13-Aptina_Focus_Test/'
-Root = '/scratch/tmp/DevWareX'
+Root = '/scratch/tmp/DevWareX/AR0130'
 
+Directories = sorted([x[0] for x in os.walk(Root)][1:])
 
-Dir = ['1391779038_0027ms_120img',
-    '1391779188_0030ms_160img',
-    '1391779241_0100ms_240img',
-    '1392645051_0018ms_240img',
-    '1392652346_0003ms_400img',
-    '1392653002_0011ms_240img']
+print 'I found these folders in', Root
+print 'Which folder do you want to analyze?'
+for item in enumerate(Directories):
+    print str(item[0]) + ':', os.path.basename(item[1]), 'containing'
+    print '   ', len(glob.glob(os.path.join(item[1], '*.raw'))), \
+        'raw and'
+    print '   ', len(glob.glob(os.path.join(item[1], '*.png'))), 'png files'
 
-Folder = os.path.join(Root, Dir[5])
+ChosenFolder = []
+while ChosenFolder not in range(len(Directories)):
+    ChosenFolder = int(raw_input('Enter [0:' + str(len(Directories) - 1) +
+                                 ']: '))
 
-# Get list of files in each folder, these are the images we acquired of which
+# Get list of files. These are the images we acquired of which
 # one has the best focus
-Images = sorted(glob.glob(os.path.join(Folder, '*.png')))
-# Discard the first five images, since they can show large inhomogeneities with
-# exposure
+Images = sorted(glob.glob(os.path.join(Directories[ChosenFolder], '*.png')))
+if not Images:
+    print 'I cannot find any png images in', \
+        os.path.basename(Directories[ChosenFolder])
+    print 'Did you save the images as RAW and not convert them yet?'
+    sys.exit('Please try again...')
+else:
+    print 'I will work with the', \
+        len(glob.glob(os.path.join(Directories[ChosenFolder], '*.png'))),\
+        'png files in', Directories[ChosenFolder]
+# Discard some images
 Images = Images[3:]
 
-print 'I found', len(Images), 'useable *.png images in', Folder
+print
+print 'I will use', len(Images), '*.png images in', Directories[ChosenFolder]
 
 # Iterate through the files, calculate the standard deviation of each image and
 # plot this.
-print 'Reading Means of images'
+print 'Calculating Mean of each of', len(Images), 'images'
 MeanExposure = [numpy.mean(plt.imread(x)) for x in Images]
-# Subtract the mean of all images and convert the now array back to a list
-MeanExposure = MeanExposure - numpy.mean(MeanExposure)
-MeanExposure = MeanExposure.tolist()
-print 'Reading Standard deviations of images'
+
+print 'Calculating standard deviation of each of', len(Images), 'of images'
 STD = [numpy.std(plt.imread(x)) for x in Images]
-STD = STD - numpy.mean(STD)
-STD = STD.tolist()
+
+normalize = True
+if normalize:
+    # Normalize the values around the mean and convert the now array back to a
+    # list
+    MeanExposure = MeanExposure - numpy.mean(MeanExposure)
+    MeanExposure = MeanExposure.tolist()
+    STD = STD - numpy.mean(STD)
+    STD = STD.tolist()
 
 plt.figure(figsize=[16, 9])
 plt.subplot(311)
 plt.title(' '.join([str(len(Images)), 'Images read from',
-    os.path.basename(Folder)]))
+    os.path.basename(Directories[ChosenFolder])]))
 # Plot values
 plt.plot(MeanExposure, color='r', alpha=0.5,
     label='Exposure with Max @ Img. ' + \
@@ -64,8 +83,13 @@ plt.plot(STD, color='b', alpha=0.5,
 # Print details and plot positions of 'Details' chosen images
 Details = 10
 for i in range(1, len(Images), int(round(len(Images) / Details))):
-    print 'Image', i, 'has a mean exposure of', round(MeanExposure[i], 3),\
-        'and a STD of', round(STD[i], 3)
+    print str(i).zfill(2), '|',
+    if normalize:
+        print 'normalized',
+    print 'Exp', str(round(MeanExposure[i], 4)), '|',
+    if normalize:
+        print 'normalized',
+    print 'STD of', round(STD[i], 4)
     plt.plot(i, MeanExposure[i], color='r', marker='.')
     plt.plot(i, STD[i], color='b', marker='.')
     plt.annotate(i, xy=(i, STD[i]), xytext=(0, 15),
@@ -107,5 +131,5 @@ plt.title('worst@' + os.path.basename(Images[STD.index(min(STD))]))
 plt.subplot(3, 2, 6)
 plt.imshow(plt.imread(Images[STD.index(max(STD))]), cmap='gray')
 plt.title('best@' + os.path.basename(Images[STD.index(max(STD))]))
-plt.savefig(Folder + '.png')
+plt.savefig(Directories[ChosenFolder] + '.png')
 plt.show()
