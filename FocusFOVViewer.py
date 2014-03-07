dc86cf9e8c7f7a6d5c57a0cac4d75520ae35ca1d
David Haberthür <email@davidhaberthuer.ch>
2014-03-07 18:04:00 +0100

    Expanding it to read all images in a given dir
    Script now reads all images in the given directory and displays them one
    after another. Extracting image size, lens and other stuff from the file
    name through some string massaging.

--- FocusFOVViewer.py
+++ FocusFOVViewer.py
@@ -5,22 +5,43 @@ Images in the given folder were saved with the
 """
 
 import os
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
 Root = "/afs/psi.ch/project/EssentialMed/Images/13-Aptina_Focus_Test/FocusFOV"
 Sensor = "AR0130"
-Image = "AR0130_1280x964_DSL315B_93.0mm_0.0_0.0f_166ms.raw"
+Sensor = "AR0132"
+Sensor = "MT9M001"
 
-ImageFile = os.path.join(Root, Sensor, Image)
+Images = sorted(glob.glob(os.path.join(Root, Sensor, '*.raw')))
+Height = [int(os.path.basename(i).split('_')[1].split('x')[1]) for i in Images]
+Width = [int(os.path.basename(i).split('_')[1].split('x')[0]) for i in Images]
+Lens = [os.path.basename(i).split('_')[2] for i in Images]
+SDD = [float(os.path.basename(i).split('_')[3].split('mm')[0]) for i in Images]
+Focus = [float(os.path.basename(i).split('_')[4]) for i in Images]
+Aperture = [float(os.path.basename(i).split('_')[5][:-1]) for i in Images]
+ExposureTime = [os.path.basename(i).split('_')[6].split('ms')[0]
+    for i in Images]
 
-print ImageFile
-
+print Height[1]
+print Width[1]
+print Lens[1]
+print SDD[1]
+print Focus[1]
+print Aperture[1]
+print ExposureTime[1]
 
 #~ Open the RAW files in Fiji as "16-bit Unsigned" with the "Width" and
 #~ "Height" given in the DevWare-window and with "Little-Endian byte order"
 
-rawimage = np.memmap(ImageFile, dtype=np.uint16, shape=(960, 1280))
-
-plt.imshow(rawimage, cmap=plt.cm.gray)
-plt.show()
+for i, item in enumerate(Images):
+    rawimage = np.memmap(Images[i], dtype=np.uint16,
+                         shape=(Height[i], Width[i]))
+    plt.imshow(rawimage, cmap=plt.cm.gray)
+    ImageTitle = Sensor, 'with the lens', Lens[i], 'focused to', \
+        str(Focus[i]), 'and aperture set to', str(Aperture[i]), \
+        'f\nExposure time of', str(ExposureTime[i]), \
+        'ms, giving a scintillator-headboard distance of', str(SDD[i]), 'mm'
+    plt.title(' '.join(ImageTitle))
+    plt.show()
