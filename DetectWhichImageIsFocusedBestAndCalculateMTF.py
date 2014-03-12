400937e1d6c280536b722031f59761b195c2c610
David Haberthür <email@davidhaberthuer.ch>
2014-03-12 18:17:28 +0100

    Remove all the cruft, calculate mean and STD
    Going step-by-step, I'm removing all the (for now) unnecessary functions
    to calculate the power spectral density and MTF. They will come back
    from MTF2.py, but for now we don't need them.
    The script now loads (raw) images from DevWare, calculates the mean
    (exposure) and STD and plots these values. Proceeding here to calculate
    the MTF and other niceties.

--- DetectWhichImageIsFocusedBestAndCalculateMTF.py
+++ DetectWhichImageIsFocusedBestAndCalculateMTF.py
@@ -5,144 +5,110 @@ It's based on the idea that once can use a random pattern to calculate the MTF,
 as specified by Daniels et al. in http://dx.doi.org/10.1117/12.190433, which
 was found through http://stackoverflow.com/q/18823968
 
-The script reads some images from the test system which were found to be
-focused best (with DetectWhichImageIsFocusedBest.py) and plots their respective
+The script reads images which were generated with GDX.ini in DevWare. It looks
+for the best focused one via the mean (exposure) and standard deviation of each
+image in a given (user-selected) folder. This image is then used for
+calculating the MTF, giving us some "hard facts" on the quality of the images.
 MTF
 '''
 
 from __future__ import division
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import numpy
-import scipy
-from scipy import ndimage
-import sys
 import os
 import glob
+import numpy
+import matplotlib.pyplot as plt
 
-print "Let's go"
+print "Hey ho, let's go"
 
-# 6 colors according to http://tools.medialab.sciences-po.fr/iwanthue/
-Hues = ["#C56447", "#A2C747", "#AB5CB2", "#96A8BF", "#543E3F", "#80B17D"]
-#~ Hues = ["#A35540", "#95BD56", "#9F5EAB", "#7E9A99"]
-#~ Hues = ["#97A861", "#9D6188"]
-
-
-def gaussfilter(InputImage, sigma=0.8):
-    '''
-    Apply gauss filter to input image, with a default sigma of 0.8, or an
-    user-supplied sigma
-    '''
-    return ndimage.gaussian_filter(InputImage, sigma)
-
-
-def psd(InputImage, Exponent=0.1):
-    '''
-    According to http://stackoverflow.com/a/15541995 we calculate the FFT,
-    shift it so that the low spatial freqencies are in the center. The power
-    spectral density is the square of the absolute of the FFT.
-    Power spectral density according to MATLAB: http://is.gd/YSUOeG
-    "imagesc( log10(abs(fftshift(fft2(Picture))).^2 ))"
-    According to Peter Modregger it's good if we first get rid of the
-    DC-component of the image, which means to delete the mean of the image from
-    itself
-    '''
-    #~ InputImage -= numpy.mean(InputImage)
-    FFTImg = numpy.fft.fft2(InputImage)
-    FFTShift = numpy.fft.fftshift(FFTImg)
-    return numpy.abs(FFTShift) ** Exponent
-
-
-def showImage(InputImage, height=0.618, color=Hues[0]):
-    '''
-    Display the image given as input in gray-scale, plot a 'color' line on
-    it at a given 'height'
-    '''
-    plt.imshow(InputImage, cmap='gray', interpolation='none')
-    plt.hlines(InputImage.shape[0] * height, 0, InputImage.shape[1],
-               linewidth=5, color=color, alpha=0.5)
-
-
-def showFFT(InputImage, colorh=Hues[2], colorv=Hues[3]):
-    '''
-    Show the FFT of the image and overlay a horizontal and vertical line from
-    the middle of the image to the border (with colors 'colorh' and 'colorv'
-    '''
-    plt.imshow(psd(InputImage), cmap='gray', interpolation='none')
-    plt.hlines(InputImage.shape[0] / 2, InputImage.shape[1] / 2,
-               InputImage.shape[1], linewidth=5, color=colorh, alpha=0.5)
-    plt.vlines(InputImage.shape[1] / 2, InputImage.shape[0] / 2,
-               InputImage.shape[0], linewidth=5, color=colorv, alpha=0.5)
-
-
-def plotFFT(InputImage, colorh=Hues[2], colorv=Hues[3], colorgaussh=Hues[4],
-            colorgaussv=Hues[5]):
-    '''
-    Plot first the horizontal line from the middle of the image (shape[1] / 2)
-    to the border (shape[1]:) at half the vertical height (shape[0]/2). Then
-    plot the vertical line from the middle of the image (shape[0]/2) to the
-    border (shape[0]/2:) in the middle of the horizontal length (shape[1] / 2))
-    '''
-    plt.plot(psd(InputImage)[InputImage.shape[0] / 2,
-                             InputImage.shape[1] / 2:], linestyle='-',
-             linewidth=5, color=colorh, alpha=1)
-    plt.plot(psd(gaussfilter(InputImage))[InputImage.shape[0] / 2,
-                                          InputImage.shape[1] / 2:],
-             linestyle='-', linewidth=5, color=colorgaussh, alpha=0.5)
-    plt.plot(psd(InputImage)[InputImage.shape[0] / 2:,
-                             InputImage.shape[1] / 2], linestyle='-',
-             linewidth=5, color=colorv, alpha=1)
-    plt.plot(psd(gaussfilter(InputImage))[InputImage.shape[0] / 2:,
-                                          InputImage.shape[1] / 2],
-             linestyle='-', linewidth=5, color=colorgaussv, alpha=0.5)
-
-# Read in Images
 Root = '/scratch/tmp/DevWareX/'
 
-
+# Look for sensor folders (saved in DevWare with GDX.ini -> [Python: Acquire X
+# images over a given rail distance])
+print 'Looking for sensor-folders in', Root, 'and disregarding other folders'
 SensorList = [os.path.basename(i) for
     i in sorted(glob.glob(os.path.join(Root, '*')))]
-
-
-print 'Which sensor-images do you want to look at?'
-
-for i,item in enumerate(SensorList):
-    print i, '-', item
-
+print 'Please select the sensor you want to look at'
+for i, item in enumerate(SensorList):
+    # Only look for AR130, AR132 and MT9M001 folders
+    if item.startswith('AR') or item.startswith('MT9'):
+        print i, '-', item
 Sensor = []
 while Sensor not in range(len(SensorList)):
     Sensor = int(input('Please enter a number: '))
 
-print 'Ok, I will work with the files in', os.path.join(Root,SensorList[Sensor])
-exit()
+# In this folder, look for lenses (saved as above)
+print 'Looking for lens folders in', os.path.join(Root, SensorList[Sensor])
+LensList = [os.path.basename(i) for
+    i in sorted(glob.glob(os.path.join(Root, SensorList[Sensor], '*')))]
+if len(LensList) > 1:
+    # Only let the user select if we found more than one lens folder
+    print 'Please select the lens you want to look at'
+    for i, item in enumerate(LensList):
+        print i, '-', item
+    Lens = []
+    while Lens not in range(len(LensList)):
+        Lens = int(input('Please enter a number: '))
+else:
+    Lens = 0
 
+# And finally, look for folders in which we saved the images
+print 'Looking for image folders in', os.path.join(Root, SensorList[Sensor],
+                                                    LensList[Lens])
+FolderList = [i for i in sorted(glob.glob(os.path.join(Root,
+                                                        SensorList[Sensor],
+                                                        LensList[Lens], '*')))]
+if len(FolderList) > 1:
+    # Only let the user select if we found more than one image folder
+    print 'Please select the folder of the images you want to look at'
+    for i, item in enumerate(FolderList):
+        print i, '-', os.path.basename(item)
+    Folder = []
+    while Lens not in range(len(FolderList)):
+        Lens = int(input('Please enter a number: '))
+else:
+    Folder = 0
 
-#~ ImageReal = plt.imread(os.path.join(Root, Directory, File))
+# Now we---finally---gan get a list of images to work with, yay!
+Images = [i for i in sorted(glob.glob(os.path.join(FolderList[Folder],
+                                                      '*.raw')))]
+print 'I will work with the', len(Images), '.raw files found in',\
+    FolderList[Folder]
+print
 
-FileList = ['AR0130/1392901972_AR0130_004ms_from100mm_to200mm_in25steps/AR0130_004ms_from100mm_to200mm_in25steps_132mm.png',
-    'AR0130/1393342195_AR0130_0.12_2.0f_029ms_0mm_to150mm/AR0130_0.12_2.0f_029ms_0mm_to150mm_049mm.png',
-    'AR0130/1393342395_AR0130_0.12_2.0f_044ms_0mm_to1500mm/AR0130_0.12_2.0f_044ms_0mm_to1500mm_600mm.png',
-    'AR0130/1393346142_AR0130_0.16_2.0f_030ms_0mm_to200mm/AR0130_0.16_2.0f_030ms_0mm_to200mm_036mm.png',
-    'AR0130/1393346212_AR0130_0.16_2.0f_030ms_0mm_to200mm/AR0130_0.16_2.0f_030ms_0mm_to200mm_118mm.png',
-    'AR0130/1393346258_AR0130_0.16_2.0f_019ms_0mm_to200mm/AR0130_0.16_2.0f_019ms_0mm_to200mm_088mm.png',
-    'AR0130/1393346400_AR0130_0.16_2.0f_019ms_0mm_to200mm/AR0130_0.16_2.0f_019ms_0mm_to200mm_084mm.png',
-    'A-1300/1393606205_A-1300_0.0_2.0f_022ms_150mm_to400mm/A-1300_0.0_2.0f_022ms_150mm_to400mm_354mm.png',
-    'A-1300/1393606263_A-1300_0.0_2.0f_022ms_150mm_to400mm/A-1300_0.0_2.0f_022ms_150mm_to400mm_210mm.png']
-for i, image in enumerate(FileList):
-    print 10 * '-', i + 1, '/', len(FileList) - 1, 30 * '-'
-    plt.subplot(4, len(FileList), i + 1)
-    print 'Reading', image
-    plt.imshow(plt.imread(os.path.join(Root, image)), cmap='gray')
-    plt.title(i)
-    plt.subplot(4, len(FileList), i + 1 + len(FileList))
-    print 'Calculating & plotting FFT'
-    showFFT(plt.imread(os.path.join(Root, image)))
-    plt.subplot(4, len(FileList), i + 1 + 2 * len(FileList))
-    print 'Calculating & plotting MTF'
-    plotFFT(plt.imread(os.path.join(Root, image)))
-plt.subplot(414)
-for i, image in enumerate(FileList):
-    plotFFT(plt.imread(os.path.join(Root, image)))
+# Get necessary parameters from the file names
+ImageHeight = int(os.path.basename(Images[1]).split('_')[1].split('x')[1])
+ImageWidth = int(os.path.basename(Images[1]).split('_')[1].split('x')[0])
+#~ Probably get some more parameters, but for now it's enough...
+
+# Iterate through the files, calculate the mean (exposure) and standard
+# deviation of each image and plot these values.
+print 'Calculating Mean for each of the', len(Images), 'images'
+MeanExposure = [numpy.mean(numpy.memmap(x, dtype=numpy.uint16,
+                                        shape=(ImageHeight, ImageWidth)))
+                for x in Images]
+
+print 'Calculating standard deviation for each of the', len(Images), 'images'
+STD = [numpy.std(numpy.memmap(x, dtype=numpy.uint16,
+                              shape=(ImageHeight, ImageWidth)))
+       for x in Images]
+
+normalize = True
+if normalize:
+    # Normalize the values around the mean and convert the now array back to a
+    # list. This gives more comparable numbers for both the mean and STD.
+    MeanExposure = MeanExposure - numpy.mean(MeanExposure)
+    MeanExposure = MeanExposure.tolist()
+    STD = STD - numpy.mean(STD)
+    STD = STD.tolist()
+
+# Plot this thing!
+plt.title(' '.join([str(len(Images)), 'Images read from\n',
+    os.path.basename(FolderList[Folder])]))
+plt.plot(MeanExposure, color='r', alpha=0.5,
+    label='Exposure with Max @ Img. ' + \
+    str(MeanExposure.index(max(MeanExposure))))
+plt.plot(STD, color='b', alpha=0.5,
+    label='STD with Max @ Img. ' + str(STD.index(max(STD))))
 plt.legend()
-
+plt.savefig('MTF_' + SensorList[Sensor] + '_' + LensList[Lens] + '.png')
 plt.show()
