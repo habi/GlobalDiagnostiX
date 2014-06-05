9da8d78556ce878202d9c5c7955853758322d8a0
David Haberthür <email@davidhaberthuer.ch>
2014-06-05 14:09:01 +0200

    Drawing is now (technically) correct.
    Added lens-ellipse, even though we still operate in the thin-lens
    regime.
    Changed color of the lens representation to make it less "imposing".
    
    Drawing a ellipsoid on top of the FOV to have a more "natural"
    representation of the view of each CMOS.
    Calculating the focal length via the distance from the CMOS to the lens,
    so we have (theoretically) only one variable.
    Split out second figure into subplots of top and side view.

--- lens_simulation.py
+++ lens_simulation.py
@@ -5,6 +5,9 @@ based on code from http://www.frantzmartinache.com/blog/?p=84
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+#~ import time
+
+plt.ion()
 
 
 def add_lens(z, f, diam, lbl):
@@ -15,12 +18,15 @@ def add_lens(z, f, diam, lbl):
     # - lbl:  label to identify the lens on the drawing
     ww, tw, rad = diam / 10.0, diam / 3.0, diam / 2.0
     plt.plot([z, z],    [-rad, rad],                'k', linewidth=2)
-    plt.plot([z, z + tw], [-rad, -rad + np.sign(f) * ww], 'k', linewidth=2)
-    plt.plot([z, z - tw], [-rad, -rad + np.sign(f) * ww], 'k', linewidth=2)
-    plt.plot([z, z + tw],  [rad,  rad - np.sign(f) * ww], 'k', linewidth=2)
-    plt.plot([z, z - tw],  [rad,  rad - np.sign(f) * ww], 'k', linewidth=2)
-    plt.plot([z + f, z + f], [-ww, ww], 'k', linewidth=2)
-    plt.plot([z - f, z - f], [-ww, ww], 'k', linewidth=2)
+    #~ plt.plot([z, z + tw], [-rad, -rad + np.sign(f) * ww], 'y', linewidth=2)
+    #~ plt.plot([z, z - tw], [-rad, -rad + np.sign(f) * ww], 'y', linewidth=2)
+    #~ plt.plot([z, z + tw],  [rad,  rad - np.sign(f) * ww], 'y', linewidth=2)
+    #~ plt.plot([z, z - tw],  [rad,  rad - np.sign(f) * ww], 'y', linewidth=2)
+    plt.plot([z + f, z + f], [-ww, ww], 'y', linewidth=2)
+    plt.plot([z - f, z - f], [-ww, ww], 'y', linewidth=2)
+    lens = patches.Ellipse((z, 0), width=2 * f, height=diam,
+                           facecolor='y', linewidth=2, alpha=0.125)
+    plt.gcf().gca().add_patch(lens)
     plt.text(z, rad + 5.0, lbl, fontsize=12)
     plt.text(z, rad + 2.0, 'f=' + str(int(f)), fontsize=10)
 
@@ -79,33 +85,47 @@ CMOSSize = np.array([1280 * pixelsize, 1024 * pixelsize])
 Magnification = FOVSize / CMOSSize
 
 # Draw the different sizes.
-figure = plt.figure()
+figure1 = plt.figure()
 plt.title("Sizes, head on")
+plt.show()
 
-Scintillator = patches.Rectangle((0, 0), 430, 430, facecolor='g', lw=2)
-figure.gca().add_patch(Scintillator)
+Scintillator = patches.Rectangle((0, 0), 430, 430, facecolor='g', linewidth=2)
+figure1.gca().add_patch(Scintillator)
 for x in range(3):
     for y in range(4):
         # Draw rectangles: http://is.gd/rmDuV1
         FOV = patches.Rectangle((x * FOVSize[0], y * FOVSize[1]),
-                                FOVSize[0], FOVSize[1], facecolor='g', lw=2)
-        figure.gca().add_patch(FOV)
+                                width=FOVSize[0], height=FOVSize[1],
+                                facecolor='g', linewidth=2)
+        figure1.gca().add_patch(FOV)
+        Ellipse = patches.Ellipse((FOVSize[0] / 2 - CMOSSize[0] / 2 + x *
+                                  FOVSize[0], FOVSize[1] / 2 -
+                                  CMOSSize[1] / 2 + y * FOVSize[1]),
+                                  width=FOVSize[0] / 0.618,
+                                  height=FOVSize[1] / 0.618, color='k',
+                                  alpha=0.125)
+        figure1.gca().add_patch(Ellipse)
         CMOS = patches.Rectangle((FOVSize[0] / 2 - CMOSSize[0] / 2 + x *
                                   FOVSize[0], FOVSize[1] / 2 -
                                   CMOSSize[1] / 2 + y * FOVSize[1]),
-                                 CMOSSize[0], CMOSSize[1], facecolor='b',
-                                 lw=2)
-        figure.gca().add_patch(CMOS)
+                                 width=CMOSSize[0], height=CMOSSize[1],
+                                 facecolor='b', linewidth=2)
+        figure1.gca().add_patch(CMOS)
 # Draw one more so we can get labels
 FOV = patches.Rectangle((0, 0), FOVSize[0], FOVSize[1], facecolor='g',
-                        lw=2, label='FOV')
-figure.gca().add_patch(FOV)
+                        linewidth=2, label='FOV')
+figure1.gca().add_patch(FOV)
+Ellipse = patches.Ellipse((FOVSize[0] / 2 - CMOSSize[0] / 2,
+                           FOVSize[1] / 2 - CMOSSize[1] / 2),
+                          width=FOVSize[0] / 0.618, height=FOVSize[1] / 0.618,
+                          color='k', alpha=0.125, label='Opt. Circle')
+figure1.gca().add_patch(Ellipse)
 CMOS = patches.Rectangle((FOVSize[0] / 2 - CMOSSize[0] / 2,
                           FOVSize[1] / 2 - CMOSSize[1] / 2), CMOSSize[0],
-                         CMOSSize[1], facecolor='b', lw=2, label='CMOS')
-figure.gca().add_patch(CMOS)
+                         CMOSSize[1], facecolor='b', linewidth=2, label='CMOS')
+figure1.gca().add_patch(CMOS)
 plt.legend()
-plt.axis('equal')
+plt.axis('scaled')
 plt.xlabel('Length [mm]')
 plt.ylabel('Length [mm]')
 plt.savefig("lens_simulation_sizecomparison.png")
@@ -117,8 +137,10 @@ plt.savefig("lens_simulation_sizecomparison.png")
 # 1/f = 1/do + 1/di
 # M = hi/ho = - di/do
 
-CMOSPosition = 5.
+CMOSPosition = 10.
 FOVPosition = Magnification[0] * CMOSPosition
+FocalLength = 1 / ((1 / CMOSPosition) + (1 / FOVPosition))
+
 
 print 80 * '-'
 print 'For a single FOV of', round(FOVSize[0], 2), 'x', \
@@ -129,34 +151,82 @@ print 'If the CMOS is', CMOSPosition, 'mm away from the lens, the',\
     'Scintillator had to be', round(FOVPosition), 'mm away from the lens.'
 print 'This means that the total optical length is', \
     round(CMOSPosition + FOVPosition), 'mm'
+print 'Since the CMOS is', CMOSPosition, 'mm away from the lens we thus',\
+    'get a focal length of of the lens of approximately', round(FocalLength, 2)
 
 #~ Lens
 # Draw the lens at the origin, to simplify things
 LensPosition = np.array([0])
-FocalLength = np.array([3])
+# Since we only draw *one* lens for the moment, we convert the focal length
+# calculated above to a NumPy array, so we can use the drawing code
+FocalLength = np.array([FocalLength])
 #~ FNumber = FocalLength / LensDiameter
 #~ FNumber = 1 / ( 2 * NumericalAperture)
 #~ -> 1 / ( 2 * FNumber) = NumericalAperture
 FNumber = 1.4
 NumericalAperture = 1 / (2 * FNumber)
 
-plt.figure()
-plt.title("side view")
+figure2 = plt.figure()
+plt.show()
+# Draw top view
+plt.subplot(121)
+plt.title("top view")
 # Draw CMOS and Scintillator
 plt.plot((-CMOSPosition, -CMOSPosition), (-CMOSSize[0] / 2, CMOSSize[0] / 2),
-         color='b', lw=2)
+         color='b', linewidth=2)
 plt.plot((FOVPosition, FOVPosition), (-FOVSize[0] / 2, FOVSize[0] / 2),
-         color='g', lw=2)
+         color='g', linewidth=2)
 
 # Draw Lens(es)
+LensDiameter = 2.0
 for i in range(np.size(LensPosition)):
-    add_lens(LensPosition[i], FocalLength[i], 5, "L" + str(i))
+    add_lens(LensPosition[i], FocalLength[i], LensDiameter, "L" + str(i))
+zmin, zmax = -CMOSPosition, FOVPosition
+NumberOfRays = 5
+for NA in [45]:  # in range(10):
+    propagate_beam((-CMOSPosition, 0), NA, NumberOfRays, LensPosition,
+                   FocalLength, 'src1', 'r')
+    propagate_beam((-CMOSPosition, CMOSSize[0]), NA, NumberOfRays,
+                   LensPosition, FocalLength, 'src1', 'r')
+    propagate_beam((-CMOSPosition, -CMOSSize[0]), NA, NumberOfRays,
+                   LensPosition, FocalLength, 'src1', 'r')
+    #~ propagate_beam((0, 10), NA, NumberOfRays, LensPosition, FocalLength,
+                   #~ 'src1', 'b')
+    plt.draw()
+    plt.pause(0.1)
+plt.savefig("lens_simulation_sideview.png")
 
-zmin, zmax       = -CMOSPosition, FOVPosition
-propagate_beam((-CMOSPosition, 0), 50, 15, LensPosition, FocalLength, 'src1',
-               'b')
+# Draw top view
+plt.subplot(122)
+plt.title("side view")
+# Draw CMOS and Scintillator
+plt.plot((-CMOSPosition, -CMOSPosition), (-CMOSSize[1] / 2, CMOSSize[1] / 2),
+         color='b', linewidth=2)
+plt.plot((FOVPosition, FOVPosition), (-FOVSize[1] / 2, FOVSize[1] / 2),
+         color='g', linewidth=2)
+
+for i in range(np.size(LensPosition)):
+    add_lens(LensPosition[i], FocalLength[i], LensDiameter, "L" + str(i))
+zmin, zmax = -CMOSPosition, FOVPosition
+
+for NA in [45]:  # in range(10):
+    propagate_beam((-CMOSPosition, 0), NA, NumberOfRays, LensPosition,
+                   FocalLength, 'src1', 'r')
+    propagate_beam((-CMOSPosition, CMOSSize[1]), NA, NumberOfRays,
+                   LensPosition, FocalLength, 'src1', 'r')
+    propagate_beam((-CMOSPosition, -CMOSSize[1]), NA, NumberOfRays,
+                   LensPosition, FocalLength, 'src1', 'r')
+    #~ propagate_beam((0, 10), NA, NumberOfRays, LensPosition, FocalLength,
+                   #~ 'src1', 'b')
+    plt.draw()
+    plt.pause(0.1)
+#~ propagate_beam(srcpos,          4, 500, zl, ff, 'src1', 'b')
+#~ propagate_beam((0.0, -2.),      4,  20, zl, ff, 'src1', 'r')
+#~ propagate_beam((zpup,),    0.0005,  20, zl, ff, 'src1', 'g')
+#~ propagate_beam((110,),          2,  40, zl, ff, 'DM',   'y')
 
 plt.savefig("lens_simulation_sideview.png")
+plt.ioff()
 plt.show()
 exit()
 
