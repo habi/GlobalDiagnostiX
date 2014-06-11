021e372b2886426298fb8830b029c1432dedfad6
David Haberthür <email@davidhaberthuer.ch>
2014-06-11 10:41:54 +0200

    Draw more precisely
    Removing unnecessary code at the end.
    Draw the start/end of the beams at the correct CMOS positions.
    Add proper coloring of the (3) beams.
    Change the labeling in the `add_lens` function.

--- lens_simulation.py
+++ lens_simulation.py
@@ -5,9 +5,20 @@ based on code from http://www.frantzmartinache.com/blog/?p=84
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-#~ import time
+import colorsys
 
 plt.ion()
+
+
+def plotcolors(NumberOfColors):
+    # After http://stackoverflow.com/a/9701141/323100
+    colors = []
+    for i in np.arange(0., 360., 360. / NumberOfColors):
+        hue = i / 360.
+        lightness = (50 + np.random.rand() * 10) / 100.
+        saturation = (90 + np.random.rand() * 10) / 100.
+        colors.append(colorsys.hls_to_rgb(hue, lightness, saturation))
+    return colors
 
 
 def add_lens(z, f, diam, lbl):
@@ -27,19 +38,19 @@ def add_lens(z, f, diam, lbl):
     lens = patches.Ellipse((z, 0), width=2 * f, height=diam,
                            facecolor='y', linewidth=2, alpha=0.125)
     plt.gcf().gca().add_patch(lens)
-    plt.text(z, rad + 5.0, lbl, fontsize=12)
-    plt.text(z, rad + 2.0, 'f=' + str(int(f)), fontsize=10)
+    plt.text(z, rad + 2.0, lbl + '\nf=' + str(round(f, 2)),
+             horizontalalignment='center')
 
 
-def propagate_beam(p0, NA, nr, zl, ff, lbl='', col='b'):
+def propagate_beam(p0, NA, nr, zl, ff, label='', color='b'):
     # geometrical propagation of light rays from given source parameters:
-    # - p0:  location of the source (z0, x0) along and off axis (in mm)
-    # - NA:  numerical aperture of the beam (in degrees)
-    # - nr:  number of rays to trace
-    # - zl:  array with the location of the lenses
-    # - ff:  array with the focal length of lenses
-    # - lbl: label for the nature of the source
-    # - col: color of the rays on plot
+    # - p0:    location of the source (z0, x0) along and off axis (in mm)
+    # - NA:    numerical aperture of the beam (in degrees)
+    # - nr:    number of rays to trace
+    # - zl:    array with the location of the lenses
+    # - ff:    array with the focal length of lenses
+    # - label: label for the nature of the source
+    # - color: color of the rays on plot
     apa = NA * np.pi / 180.0
     z0 = p0[0]
     if (np.size(p0) == 2):
@@ -63,7 +74,7 @@ def propagate_beam(p0, NA, nr, zl, ff, lbl='', col='b'):
 
         zz[nl + 1] = zmax
         xx[nl + 1] = xx[nl] + (zz[nl + 1] - zz[nl]) * tani[nl]
-        plt.plot(zz, xx, col)
+        plt.plot(zz, xx, color=color)
 
 FOVSize = np.array([430 / 3., 430 / 4.])
 
@@ -137,10 +148,9 @@ plt.savefig("lens_simulation_sizecomparison.png")
 # 1/f = 1/do + 1/di
 # M = hi/ho = - di/do
 
-CMOSPosition = 10.
+CMOSPosition = 25.
 FOVPosition = Magnification[0] * CMOSPosition
 FocalLength = 1 / ((1 / CMOSPosition) + (1 / FOVPosition))
-
 
 print 80 * '-'
 print 'For a single FOV of', round(FOVSize[0], 2), 'x', \
@@ -160,6 +170,7 @@ LensPosition = np.array([0])
 # Since we only draw *one* lens for the moment, we convert the focal length
 # calculated above to a NumPy array, so we can use the drawing code
 FocalLength = np.array([FocalLength])
+
 #~ FNumber = FocalLength / LensDiameter
 #~ FNumber = 1 / ( 2 * NumericalAperture)
 #~ -> 1 / ( 2 * FNumber) = NumericalAperture
@@ -176,24 +187,26 @@ plt.plot((-CMOSPosition, -CMOSPosition), (-CMOSSize[0] / 2, CMOSSize[0] / 2),
          color='b', linewidth=2)
 plt.plot((FOVPosition, FOVPosition), (-FOVSize[0] / 2, FOVSize[0] / 2),
          color='g', linewidth=2)
-
 # Draw Lens(es)
-LensDiameter = 2.0
+LensDiameter = 12.0
 for i in range(np.size(LensPosition)):
     add_lens(LensPosition[i], FocalLength[i], LensDiameter, "L" + str(i))
 zmin, zmax = -CMOSPosition, FOVPosition
-NumberOfRays = 5
-for NA in [45]:  # in range(10):
-    propagate_beam((-CMOSPosition, 0), NA, NumberOfRays, LensPosition,
-                   FocalLength, 'src1', 'r')
-    propagate_beam((-CMOSPosition, CMOSSize[0]), NA, NumberOfRays,
-                   LensPosition, FocalLength, 'src1', 'r')
-    propagate_beam((-CMOSPosition, -CMOSSize[0]), NA, NumberOfRays,
-                   LensPosition, FocalLength, 'src1', 'r')
-    #~ propagate_beam((0, 10), NA, NumberOfRays, LensPosition, FocalLength,
-                   #~ 'src1', 'b')
-    plt.draw()
-    plt.pause(0.1)
+# Draw beam paths
+c = plotcolors(5)
+NumberOfRays = 10
+BeamNA = 1
+propagate_beam((-CMOSPosition, 0), BeamNA, NumberOfRays, LensPosition,
+               FocalLength, color=c[0])
+propagate_beam((-CMOSPosition, CMOSSize[0] / 4), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[1])
+propagate_beam((-CMOSPosition, CMOSSize[0] / 2), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[2])
+propagate_beam((-CMOSPosition, -CMOSSize[0] / 4), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[3])
+propagate_beam((-CMOSPosition, -CMOSSize[0] / 2), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[4])
+plt.draw()
 plt.savefig("lens_simulation_sideview.png")
 
 # Draw top view
@@ -204,60 +217,22 @@ plt.plot((-CMOSPosition, -CMOSPosition), (-CMOSSize[1] / 2, CMOSSize[1] / 2),
          color='b', linewidth=2)
 plt.plot((FOVPosition, FOVPosition), (-FOVSize[1] / 2, FOVSize[1] / 2),
          color='g', linewidth=2)
-
+# Draw Lens(es)
 for i in range(np.size(LensPosition)):
     add_lens(LensPosition[i], FocalLength[i], LensDiameter, "L" + str(i))
-zmin, zmax = -CMOSPosition, FOVPosition
-
-for NA in [45]:  # in range(10):
-    propagate_beam((-CMOSPosition, 0), NA, NumberOfRays, LensPosition,
-                   FocalLength, 'src1', 'r')
-    propagate_beam((-CMOSPosition, CMOSSize[1]), NA, NumberOfRays,
-                   LensPosition, FocalLength, 'src1', 'r')
-    propagate_beam((-CMOSPosition, -CMOSSize[1]), NA, NumberOfRays,
-                   LensPosition, FocalLength, 'src1', 'r')
-    #~ propagate_beam((0, 10), NA, NumberOfRays, LensPosition, FocalLength,
-                   #~ 'src1', 'b')
-    plt.draw()
-    plt.pause(0.1)
-#~ propagate_beam(srcpos,          4, 500, zl, ff, 'src1', 'b')
-#~ propagate_beam((0.0, -2.),      4,  20, zl, ff, 'src1', 'r')
-#~ propagate_beam((zpup,),    0.0005,  20, zl, ff, 'src1', 'g')
-#~ propagate_beam((110,),          2,  40, zl, ff, 'DM',   'y')
+# Draw beam paths
+propagate_beam((-CMOSPosition, 0), BeamNA, NumberOfRays, LensPosition,
+               FocalLength, color=c[0])
+propagate_beam((-CMOSPosition, CMOSSize[1] / 4), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[1])
+propagate_beam((-CMOSPosition, CMOSSize[1] / 2), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[2])
+propagate_beam((-CMOSPosition, -CMOSSize[1] / 4), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[3])
+propagate_beam((-CMOSPosition, -CMOSSize[1] / 2), BeamNA, NumberOfRays,
+               LensPosition, FocalLength, color=c[4])
+plt.draw()
 
 plt.savefig("lens_simulation_sideview.png")
 plt.ioff()
-plt.show()
-exit()
-
-# Draw the light beams
-xsrc, zsrc, zpup = 15.0, 15.0, -10  # position of src and pupil
-srcpos = (zsrc, xsrc)
-
-#  draw the different beams
-# --------------------------
-propagate_beam((0, CMOSSize[0]), NumericalAperture, 30, LensPosition,
-                FocalLength, 'src1', 'b')
-propagate_beam((0.0, 0), 6,  20, LensPosition, FocalLength, 'src1', 'r')
-propagate_beam((zpup,), 0.0005,  20, LensPosition, FocalLength, 'src1', 'g')
-propagate_beam((110,), 50,  40, LensPosition, FocalLength, 'DM',   'y')
-
-#  print a couple labels
-# --------------------------
-plt.text(0, 20, 'src 1', bbox=dict(facecolor='blue', alpha=1), fontsize=10)
-plt.text(0, 17, 'src 2', bbox=dict(facecolor='red',  alpha=1), fontsize=10)
-plt.text(0, 14, 'pupil', bbox=dict(facecolor='green',  alpha=1), fontsize=10)
-plt.text(0, 11, 'DM', bbox=dict(facecolor='yellow',  alpha=1), fontsize=10)
-#~
-#      add the lenses
-# -------------------------
-for i in range(np.size(LensPosition)):
-    add_lens(LensPosition[i], FocalLength[i], 10, "L" + str(i))
-
-#     plot optical axis
-# -------------------------
-#~ plt.plot([zmin,zmax], [0,0], 'k')
-plt.plot()
-#~ plt.axis([zmin,zmax, xmin, xmax])
-plt.title("Example of brilliant optical design!")
 plt.show()
