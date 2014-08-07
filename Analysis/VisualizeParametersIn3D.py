1781994e0ffab003a5dcfe3ab8358d224da65258
David Haberthür <email@davidhaberthuer.ch>
2014-08-07 18:28:26 +0200

    Select lens (or other variable) with masked arrays
    Using a masked array, we select only one lens/exposure time/other
    variable to display. While doing this, we found out that some of the
    images are saved with strange bit depth, leading to the update in
    3e49d69...

--- Analysis/VisualizeParametersIn3D.py
+++ Analysis/VisualizeParametersIn3D.py
@@ -14,12 +14,7 @@ import os
 import linecache
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-
-#~ import matplotlib as mpl
-#~ import numpy as np
-#~ from matplotlib import cm
-
+import numpy
 
 # Where shall we start?
 StartingFolder = ('/afs/psi.ch/project/EssentialMed/MasterArbeitBFH/' +
@@ -44,7 +39,7 @@ Sensor = [linecache.getline(i, 7).split(':')[1].strip()
     for i in LogFiles]
 Scintillator = [linecache.getline(i, 6).split(':')[1].strip()
     for i in LogFiles]
-Lens = [linecache.getline(i, 8).split(':')[1].strip() for i in LogFiles]
+Lens = [str(linecache.getline(i, 8).split(':')[1].strip()) for i in LogFiles]
 SSD = [float(linecache.getline(i, 10).split(':')[1].split('mm')[0].strip())
     for i in LogFiles]
 Modality = [linecache.getline(i, 11).split(':')[1].strip()
@@ -58,15 +53,50 @@ Mean = [float(linecache.getline(i, 23).split(':')[1].strip())
 STD = [float(linecache.getline(i, 24).split(':')[1].strip())
     for i in LogFiles]
 
-NormalizedMax = [x/max(Max) for x in Max]
-NormalizedMean = [x/max(Mean) for x in Mean]
-NormalizedSTD = [x/max(STD) for x in STD]
+# Information about what we did
+print 'In these log files, we have data for'
+print '\t- ', len(set(Scintillator)), 'scintillators: ',
+for i in set(Scintillator):
+    print i + ',',
+print '\n\t- ', len(set(Sensor)), 'sensors:',
+for i in set(Sensor):
+    print i + ',',
+print '\n\t- ', len(set(Lens)), 'lenses:',
+for i in set(Lens):
+    print i + ',',
+print '\n\t- ', len(set(Modality)), 'modalities:',
+for i in set(Modality):
+    print i + ',',
+print
 
+# Possibility to select only one value
+SelectedLens = 'Lensation-CVM0411ND'
+Selection = numpy.ma.masked_where(numpy.asarray(Lens) != SelectedLens, Lens)
+print
+print 'If we select only the', SelectedLens, 'lens we display', \
+    len(Selection.compressed()), 'experiments with the IDs'
+for i in numpy.ma.masked_array(ExperimentID, Selection.mask).compressed():
+    print '\t-', i
+    print '\t\tin folder', \
+        os.path.dirname(LogFiles[ExperimentID.index(i)])[len(StartingFolder) +  1:]
+    print '\t\twith a mean of', Mean[ExperimentID.index(i)]
+
+# Display information about the selected experiments
+
+# Plot figure
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.scatter(Exposuretime, SSD, Mean, 'o', c=Mean, cmap='gray', s=150)
-for x, y, z, label in zip(Exposuretime, SSD, Mean, zip(ExperimentID)):
-    ax.text(x, y, z + 0.05, '\n'.join(label))
+plot = ax.scatter(
+    numpy.ma.masked_array(Exposuretime, Selection.mask),
+    numpy.ma.masked_array(SSD, Selection.mask),
+    numpy.ma.masked_array(Mean, Selection.mask),
+    'o', c=Mean, cmap='hot', s=250)
+for x, y, z, label in zip(
+    numpy.ma.masked_array(Exposuretime, Selection.mask),
+    numpy.ma.masked_array(SSD, Selection.mask),
+    numpy.ma.masked_array(Mean, Selection.mask),
+    zip(Scintillator, ExperimentID)):
+    ax.text(x, y, z + 0.05, '|'.join(label))
 
 ax.set_xlabel('Exposure time [ms]')
 ax.set_ylabel('Scintillator-CMOS distance [mm]')
@@ -74,6 +104,8 @@ ax.set_zlabel('Mean brightness of brightest image')
 #~ ax.set_xlim([0,100]
 #~ ax.set_ylim([0,750])
 #~ ax.set_zlim([0,100])
+
+cbar = fig.colorbar(plot, ticks=[min(Mean), 0, max(Mean)])
 plt.title(' '.join(['Data from', str(len(LogFiles)), 'log files']))
 
 plt.show()
