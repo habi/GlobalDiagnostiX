f35c1cadb4707d341c52aeebbeabf3d7e7d54e1f
David Haberthür <email@davidhaberthuer.ch>
2014-08-19 15:52:33 +0200

    Resolution-Visualization script shows lines correctly
    The AnalyzeGrid script read (for the moment) one image, makes the user
    select a ROI and plots the brightness values in this ROI.
    
    This makes it possible to visualize the resolution phantom, i.e. detect
    if we can distinguish xx lp/mm.

--- Analysis/AnalyzeGrid.py
+++ Analysis/AnalyzeGrid.py
@@ -43,9 +43,12 @@ def contrast_stretch(image):
 
 ReadRaw = True
 if ReadRaw:
-    Directory = '/afs/psi.ch/user/h/haberthuer/EssentialMed/MasterArbeitBFH/XrayImages/20140731/Toshiba/AR0132/TIS-TBL-6C-3MP/Hand'
+    Directory = '/afs/psi.ch/user/h/haberthuer/EssentialMed/' + \
+        'MasterArbeitBFH/XrayImages/20140731/Toshiba/AR0132/' + \
+        'TIS-TBL-6C-3MP/Hand'
     Folder = '5808858'
-    File = '5808858_Toshiba_AR0132_1280x964_TIS-TBL-6C-3MP_190mmSDD_Hand_60kV_2mAs_25.0msXray_65.99msCMOS_11.raw'
+    File = '5808858_Toshiba_AR0132_1280x964_TIS-TBL-6C-3MP_190mmSDD_Hand_' + \
+        '60kV_2mAs_25.0msXray_65.99msCMOS_11.raw'
     Size = [int(File.split('_')[3].split('x')[1]),
         int(File.split('_')[3].split('x')[0])]
     # Load Image
@@ -62,7 +65,9 @@ if ReadRaw:
     # bottom
     Image[-padwidth:, :] = 0
 else:
-    Directory = '/afs/psi.ch/user/h/haberthuer/EssentialMed/MasterArbeitBFH/XrayImages/20140731/Toshiba/AR0132/TIS-TBL-6C-3MP/Hand'
+    Directory = '/afs/psi.ch/user/h/haberthuer/EssentialMed/' + \
+        'MasterArbeitBFH/XrayImages/20140731/Toshiba/AR0132/' + \
+        'TIS-TBL-6C-3MP/Hand'
     Folder = ''
     File = 'Analysis_5808858_Corrected.png'
     FileToLoad = os.path.join(Directory, Folder, File)
@@ -95,27 +100,25 @@ plt.hist(StretchedImage.flatten(), 64)
 plt.xlim([0, 256])
 plt.tight_layout()
 
-plt.figure('Selection')
+plt.figure('Selection', figsize=[23, 9])
 plt.subplot(121)
 plt.imshow(StretchedImage, cmap='bone', interpolation='nearest')
 
 # Let the user select the ROI of the resolution phantom
-tellme('Click with the mouse to start')
-plt.waitforbuttonpress()
 done = False
 while not done:
     pts = []
     while len(pts) < 2:
         tellme('Select 2 diagonal edges of the resolution phantom')
-        pts = numpy.asarray( plt.ginput(2,timeout=-1) )
+        pts = numpy.asarray(plt.ginput(2, timeout=-1))
         if len(pts) < 2:
             tellme('Too few points, starting over')
-            time.sleep(1) # Wait a second
+            time.sleep(1)  # Wait a second
     # Get region of interest from user input and draw it
-    xmin = min(pts[:,0])
-    xmax = max(pts[:,0])
-    ymin = min(pts[:,1])
-    ymax = max(pts[:,1])
+    xmin = min(pts[:, 0])
+    xmax = max(pts[:, 0])
+    ymin = min(pts[:, 1])
+    ymax = max(pts[:, 1])
     currentAxis = plt.gca()
     rectangle = currentAxis.add_patch(Rectangle((xmin, ymin), xmax - xmin,
                                                 ymax - ymin, facecolor='red',
@@ -132,119 +135,63 @@ tellme('Selected ROI')
 plt.subplot(222)
 CroppedImage = contrast_stretch(Image[ymin:ymax, xmin:xmax])
 plt.imshow(CroppedImage, cmap='bone', interpolation='none')
-tellme(' '.join(['ROI:', str(int(round(xmax-xmin))), 'x',
-    str(int(round(ymax-ymin))), 'px']))
+tellme(' '.join(['ROI:', str(int(round(xmax - xmin))), 'x',
+    str(int(round(ymax - ymin))), 'px']))
 plt.tight_layout()
-# Select horizontal line to plot
+# Select ROI of resolution phantom. Afterwards we draw $steps horizontal lines
+# in a region $pad px bigger than the selected ROI
+pad = 25
+steps = 5
 done = False
 while not done:
     pts = []
-    while len(pts) < 1:
-        tellme('Select height of grid positions')
-        pts = numpy.asarray( plt.ginput(1,timeout=-1) )
-        if len(pts) < 1:
-            tellme('Try again')
-            time.sleep(1) # Wait a second
+    while len(pts) < 2:
+        tellme('Select opposite edges of resolution phantom region')
+        pts = numpy.asarray(plt.ginput(2, timeout=-1))
+        if len(pts) < 2:
+            tellme('Too few points, starting over')
+            time.sleep(1)  # Wait a second
     # Get region of interest from user input and draw it
-    plt.axhline(pts[:,1], linewidth=4, color='k', alpha=0.5)
+    xmin = min(pts[:, 0])
+    xmax = max(pts[:, 0])
+    ymin = min(pts[:, 1])
+    ymax = max(pts[:, 1])
+    currentAxis = plt.gca()
+    rectangle = currentAxis.add_patch(Rectangle((xmin - pad, ymin),
+                                                xmax - xmin + pad + pad,
+                                                ymax - ymin, facecolor='red',
+                                                alpha=0.25))
     tellme('Done? Press any key for yes, click with mouse for no')
     done = plt.waitforbuttonpress()
-    # Get rid of fill
+    # Redraw image if necessary
     if not done:
         plt.clf()
         plt.subplot(222)
-        plt.imshow(CroppedImage, origin='upper', interpolation='none')
-tellme('Selected line')
+        plt.imshow(CroppedImage, cmap='bone', interpolation='none')
+tellme('Selected ROI and lines of plot below')
 
-# draw five horizontal lines 20 px around the selected one
-# IWantHue, dark background, 5 colors, hard
-clr=["#53F5F4", "#3F235B", "#5D8A1A", "#F6B08A", "#BBBBF9"]
-Phantom = []
-for c, i in enumerate(numpy.linspace(-20,20,5)):
-    plt.subplot(222)
-    plt.axhline(i + pts[:,1], linewidth=4, alpha = 0.5, color=clr[c])
-    plt.subplot(224)
-    plt.plot(CroppedImage[i + int(round(pts[:,1])),:], color=clr[c])
-    #~ Phantom = Phantom + CroppedImage[i + int(round(pts[:,1])),:]
-#~ print Phantom
-#~ plt.plot(numpy.mean(Phantom), 'k', linewidth='10', label='mean', alpha=1)
-#~ plt.legend(loc='best')
+# draw $steps horizontal lines $pad px around the selected one
+# IWantHue, dark background, 10 colors, hard
+clr = ["#6B9519", "#9B46C3", "#281B32", "#F29C2F", "#F0418C", "#8DF239",
+    "#EBF493", "#4680F0", "#402305", "#9F9BFD"]
+Phantom = numpy.empty(shape=(int(round((xmax + pad) - (xmin - pad))),
+                             steps))
+SelectedHeight = numpy.linspace(ymin, ymax, steps)
+SelectedLines = [line for
+    line in [CroppedImage[int(round(height)), xmin - pad:xmax + pad] for
+    height in SelectedHeight]]
 
+plt.subplot(222)
+for c, height in enumerate(SelectedHeight):
+    plt.axhline(height, linewidth=4, alpha=0.5, color=clr[c])
+plt.subplot(224)
+for c, line in enumerate(SelectedLines):
+    plt.plot(line, alpha=0.5, color=clr[c])
+plt.plot(numpy.mean(SelectedLines, axis=0), 'k', linewidth='4',
+    label=' '.join(['mean of', str(steps), 'shown lines']))
+plt.legend(loc='best')
+plt.title('Brightness in the red ROI shown above')
+plt.tight_layout()
 
 plt.ioff()
-plt.show()
-exit()
-
-
-
-
-##################################################
-# Define a triangle by clicking three points
-##################################################
-plt.clf()
-plt.axis([-1.,1.,-1.,1.])
-plt.setp(plt.gca(),autoscale_on=False)
-
-
-
-plt.waitforbuttonpress()
-
-happy = False
-while not happy:
-    pts = []
-    while len(pts) < 3:
-        tellme('Select 3 corners with mouse')
-        pts = np.asarray( plt.ginput(3,timeout=-1) )
-        if len(pts) < 3:
-            tellme('Too few points, starting over')
-            time.sleep(1) # Wait a second
-
-    ph = plt.fill( pts[:,0], pts[:,1], 'r', lw=2 )
-
-    tellme('Happy? Key click for yes, mouse click for no')
-
-    happy = plt.waitforbuttonpress()
-
-    # Get rid of fill
-    if not happy:
-        for p in ph: p.remove()
-
-##################################################
-# Now contour according to distance from triangle
-# corners - just an example
-##################################################
-
-# Define a nice function of distance from individual pts
-def f(x,y,pts):
-    z = np.zeros_like(x)
-    for p in pts:
-        z = z + 1/(np.sqrt((x-p[0])**2+(y-p[1])**2))
-    return 1/z
-
-X,Y = np.meshgrid( np.linspace(-1,1,51), np.linspace(-1,1,51) )
-Z = f(X,Y,pts)
-
-CS = plt.contour( X, Y, Z, 20 )
-
-tellme( 'Use mouse to select contour label locations, middle button to finish' )
-CL = plt.clabel( CS, manual=True )
-
-##################################################
-# Now do a zoom
-##################################################
-tellme( 'Now do a nested zoom, click to begin' )
-plt.waitforbuttonpress()
-
-happy = False
-while not happy:
-    tellme( 'Select two corners of zoom, middle mouse button to finish' )
-    pts = np.asarray( plt.ginput(2,timeout=-1) )
-
-    happy = len(pts) < 2
-    if happy: break
-
-    pts = np.sort(pts,axis=0)
-    plt.axis( pts.T.ravel() )
-
-tellme('All Done!')
 plt.show()
