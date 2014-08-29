93cce1841f4f9ffafcb3590ea305ea9812f1d13d
David Haberthür <email@davidhaberthuer.ch>
2014-08-29 17:33:45 +0200

    Adapt to 'new' log file
    We've changed the logfile output, so we needed to change where we read
    the data from.
    Adapted output to be a bit nicer.
    Figure is prepared to show two different parameter sets, we only need to
    decide which one it shall be.
    Saving image at the end.

--- Analysis/VisualizeParametersIn3D.py
+++ Analysis/VisualizeParametersIn3D.py
@@ -19,56 +19,75 @@ import numpy
 # Where shall we start?
 RootFolder = ('/afs/psi.ch/project/EssentialMed/MasterArbeitBFH/' +
     'XrayImages')
+StartingFolder = os.path.join(RootFolder, '20140721')
+#~ StartingFolder = os.path.join(RootFolder, '20140722')
+#~ StartingFolder = os.path.join(RootFolder, '20140724')
+#~ StartingFolder = os.path.join(RootFolder, '20140730')
+#~ StartingFolder = os.path.join(RootFolder, '20140731')
+#~ StartingFolder = os.path.join(RootFolder, '20140818')
+#~ StartingFolder = os.path.join(RootFolder, '20140819')
+#~ StartingFolder = os.path.join(RootFolder, '20140820')
+#~ StartingFolder = os.path.join(RootFolder, '20140822')
+#~ StartingFolder = os.path.join(RootFolder, '20140823')
+#~ StartingFolder = os.path.join(RootFolder, '20140825')
 StartingFolder = os.path.join(RootFolder)
 
 # Generate a list of log files, based on http://stackoverflow.com/a/14798263
 LogFiles = [os.path.join(dirpath, f)
     for dirpath, dirnames, files in os.walk(StartingFolder)
-    for f in files if f.startswith('Analysis') and f.endswith('.log')]
+    for f in files if f.endswith('analysis.log')]
 
-LogFiles = LogFiles[:-100]
 print 'I found', len(LogFiles), 'log files in', StartingFolder
 
 # Grab all the necessary parameters from the log files
-ExperimentID = [linecache.getline(i, 1).split('ID')[1].split(',')[0].strip()
+ExperimentID = \
+    [int(linecache.getline(i, 1).split('ID')[1].split(',')[0].strip())
     for i in LogFiles]
-Sensor = [linecache.getline(i, 7).split(':')[1].strip()
+Sensor = [linecache.getline(i, 10).split(':')[1].strip() for i in LogFiles]
+Scintillator = [linecache.getline(i, 9).split(':')[1].strip()
     for i in LogFiles]
-Scintillator = [linecache.getline(i, 6).split(':')[1].strip()
+Lens = [str(linecache.getline(i, 11).split(':')[1].strip()) for i in LogFiles]
+SSD = [float(linecache.getline(i, 13).split(':')[1].split('mm')[0].strip())
     for i in LogFiles]
-Lens = [str(linecache.getline(i, 8).split(':')[1].strip()) for i in LogFiles]
-SSD = [float(linecache.getline(i, 10).split(':')[1].split('mm')[0].strip())
+Modality = [linecache.getline(i, 14).split(':')[1].strip()
     for i in LogFiles]
-Modality = [linecache.getline(i, 11).split(':')[1].strip()
-    for i in LogFiles]
-Exposuretime = [float(linecache.getline(i, 15)
+Exposuretime = [float(linecache.getline(i, 18)
     .split(':')[1].split('ms')[0].strip()) for i in LogFiles]
-Max = [float(linecache.getline(i, 22).split(':')[1].strip())
+
+#~ for i in LogFiles:
+    #~ print i
+    #~ print float(linecache.getline(i, 25).split(':')[1].strip())
+    #~ print 80*'-'
+#~ exit()
+
+Max = [float(linecache.getline(i, 25).split(':')[1].strip())
     for i in LogFiles]
-Mean = [float(linecache.getline(i, 23).split(':')[1].strip())
+Mean = [float(linecache.getline(i, 26).split(':')[1].strip())
     for i in LogFiles]
-STD = [float(linecache.getline(i, 24).split(':')[1].strip())
+STD = [float(linecache.getline(i, 27).split(':')[1].strip())
     for i in LogFiles]
 
 # Information about what we did
 print 'In these log files, we have data for'
-print '\t- ', len(set(Scintillator)), 'scintillators: ',
+print '\t-', len(set(Scintillator)), 'scintillators:'
 for i in set(Scintillator):
-    print i + ',',
-print '\n\t- ', len(set(Sensor)), 'sensors:',
+    print '\t\t-', i
+print '\t-', len(set(Sensor)), 'sensors:'
 for i in set(Sensor):
-    print i + ',',
-print '\n\t- ', len(set(Lens)), 'lenses:',
+    print '\t\t-', i
+print '\t-', len(set(Lens)), 'lenses:'
 for i in numpy.unique(Lens):
-    print i + ',',
-print '\n\t- ', len(set(Modality)), 'modalities:',
+    print '\t\t-', i
+print '\t- ', len(set(Modality)), 'modalities:'
 for i in set(Modality):
-    print i + ',',
-print
+    print '\t\t-', i
 
 # Plot figure
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+## CMOS-Distance
+fig = plt.figure(figsize=[16, 9])
+fig.suptitle(' '.join(['Data from', str(len(LogFiles)),
+    'log files from', StartingFolder, 'colored by STD']))
+ax = fig.add_subplot(121, projection='3d')
 plot = ax.scatter(
     SSD, Mean, Max,
     'o', c=STD, cmap='hot', s=250)
@@ -77,16 +96,34 @@ for x, y, z, label in zip(
     zip(Modality)):
     ax.text(x, y + 0.2, z + 0.2, '|'.join(label))
 
-ax.set_xlabel('Scintillator-CMOS distance [mm]')
-ax.set_ylabel('Mean Brightness of best image')
-ax.set_zlabel('Max Brightness of best image')
-
 #~ ax.set_xlim([0,333])
 #~ ax.set_ylim([0,750])
 #~ ax.set_zlim([0,100])
 
-cbar = fig.colorbar(plot, ticks=[min(Mean), 0, max(Mean)])
-plt.title(' '.join(['Data from', str(len(LogFiles)),
-    'log files, colored by "Standard Deviation"']))
+ax.set_xlabel('Scintillator-CMOS distance [mm]')
+ax.set_ylabel('Mean Brightness of best image')
+ax.set_zlabel('Max Brightness of best image')
+plt.title('Modality')
+
+##
+ax = fig.add_subplot(122, projection='3d')
+plot = ax.scatter(
+    ExperimentID, Exposuretime, Max,
+    'o', c=STD, cmap='hot', s=250)
+#~ for x, y, z, label in zip(
+    #~ SSD, Mean, Max,
+    #~ zip(Lens)):
+    #~ ax.text(x, y + 0.2, z + 0.2, '|'.join(label))
+
+ax.set_xlabel('id')
+ax.set_ylabel('exp time')
+ax.set_zlabel('max')
+plt.title('something')
+
+plt.tight_layout()
+
+OutputImage = os.path.join(StartingFolder, '_Overview.png')
+print 'Saving figure as', OutputImage
+plt.savefig(OutputImage, transparent=True)
 
 plt.show()
