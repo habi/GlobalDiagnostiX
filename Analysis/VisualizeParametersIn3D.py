4cf2d3727fe69ce0b5b1fc9d5fbb8b7bb339324d
David Haberthür <email@davidhaberthuer.ch>
2014-09-02 17:28:49 +0200

    Show only a subset of values
    Maybe it would be better to use a masked array or a dictionary, but by
    using the idea presented in http://stackoverflow.com/a/3555387/323100 we
    can *easily* show only a subset of values, currently only the
    experiments with the AppScinTech-szintillator.
    
    Starting to optimize presentation with semi-transparent labels and the
    like.
    There's still *lots* of work in this departement :)
    
    Also removing the /usr/bin/python to possibly help with Ivans homebrewn
    python installation

--- Analysis/VisualizeParametersIn3D.py
+++ Analysis/VisualizeParametersIn3D.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 """
@@ -53,19 +52,17 @@ Modality = [linecache.getline(i, 14).split(':')[1].strip()
     for i in LogFiles]
 Exposuretime = [float(linecache.getline(i, 18)
     .split(':')[1].split('ms')[0].strip()) for i in LogFiles]
-
-#~ for i in LogFiles:
-    #~ print i
-    #~ print float(linecache.getline(i, 25).split(':')[1].strip())
-    #~ print 80*'-'
-#~ exit()
-
 Max = [float(linecache.getline(i, 25).split(':')[1].strip())
     for i in LogFiles]
 Mean = [float(linecache.getline(i, 26).split(':')[1].strip())
     for i in LogFiles]
 STD = [float(linecache.getline(i, 27).split(':')[1].strip())
     for i in LogFiles]
+#~ for i in LogFiles:
+    #~ print i
+    #~ print float(linecache.getline(i, 25).split(':')[1].strip())
+    #~ print 80*'-'
+#~ exit()
 
 # Information about what we did
 print 'In these log files, we have data for'
@@ -90,40 +87,50 @@ fig.suptitle(' '.join(['Data from', str(len(LogFiles)),
 ax = fig.add_subplot(121, projection='3d')
 plot = ax.scatter(
     SSD, Mean, Max,
-    'o', c=STD, cmap='hot', s=250)
+    'o', c=STD, edgecolor='', cmap='hot', s=250, alpha=0.5)
 for x, y, z, label in zip(
     SSD, Mean, Max,
-    zip(Modality)):
-    ax.text(x, y + 0.2, z + 0.2, '|'.join(label))
+    zip(Sensor, Lens)):
+    ax.text(x, y + 0.2, z + 0.2, ' / '.join(label), alpha=0.25)
 
-#~ ax.set_xlim([0,333])
-#~ ax.set_ylim([0,750])
-#~ ax.set_zlim([0,100])
+#~ ax.set_xlim([66, 222])
+#~ ax.set_ylim([0, 750])
+#~ ax.set_zlim([0, 1000])
 
 ax.set_xlabel('Scintillator-CMOS distance [mm]')
 ax.set_ylabel('Mean Brightness of best image')
 ax.set_zlabel('Max Brightness of best image')
 plt.title('Modality')
 
-##
+# Select only a subset of items to present in the second plot, according to
+# http://stackoverflow.com/a/3555387/323100
+MaskedX = [item for item, flag in zip(Max, Scintillator) if 'AppS' in flag]
+MaskedY = [item for item, flag in zip(Mean, Scintillator) if 'AppS' in flag]
+MaskedZ = [item for item, flag in zip(STD, Scintillator) if 'AppS' in flag]
+MaskedC = [item for item, flag in zip(STD, Scintillator) if 'AppS' in flag]
+MaskedI = [str(item) for item, flag in zip(ExperimentID, Scintillator) if
+    'AppS' in flag]
+
+## AppScinTech
 ax = fig.add_subplot(122, projection='3d')
 plot = ax.scatter(
-    ExperimentID, Exposuretime, Max,
-    'o', c=STD, cmap='hot', s=250)
-#~ for x, y, z, label in zip(
-    #~ SSD, Mean, Max,
-    #~ zip(Lens)):
-    #~ ax.text(x, y + 0.2, z + 0.2, '|'.join(label))
+    MaskedX, MaskedY, MaskedZ,
+    'o', c=MaskedC, cmap='hot', s=250)
+for x, y, z, label in zip(
+    MaskedX, MaskedY, MaskedZ,
+    zip(MaskedI)):
+    ax.text(x, y + 0.2, z + 0.2, ' / '.join(label), alpha=0.25)
 
-ax.set_xlabel('id')
-ax.set_ylabel('exp time')
-ax.set_zlabel('max')
-plt.title('something')
+ax.set_xlabel('Max')
+ax.set_ylabel('Mean')
+ax.set_zlabel('STD')
+plt.title(' '.join([str(len(MaskedX)),
+    'values where Scintillator=AppScinTech']))
 
 plt.tight_layout()
 
 OutputImage = os.path.join(StartingFolder, '_Overview.png')
 print 'Saving figure as', OutputImage
-plt.savefig(OutputImage, transparent=True)
+plt.savefig(OutputImage, transparent=False)
 
 plt.show()
