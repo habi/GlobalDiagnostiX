d1d207edc1347eb778d3b5d74dd1d08393e315b2
David Haberthür <email@davidhaberthuer.ch>
2014-09-25 17:05:47 +0200

    Give some more information, adapt plot
    We output the values for exposure time, sdd, mean, max and STD
    including a histogram.
    Set all the plots to have the same axis limits.
    Export rotating frames, so we have a movie to show in the progress
    report.

--- Analysis/VisualizeParametersIn3D.py
+++ Analysis/VisualizeParametersIn3D.py
@@ -21,23 +21,26 @@ if 'linux' in sys.platform:
     # If running at the office, grep AFS
     RootFolder = ('/afs/psi.ch/project/EssentialMed/MasterArbeitBFH/' +
         'XrayImages')
-    #~ StartingFolder = os.path.join(RootFolder, '20140721')  # 11
-    #~ StartingFolder = os.path.join(RootFolder, '20140722')  # 44
-    #~ StartingFolder = os.path.join(RootFolder, '20140724')  # 91
-    #~ StartingFolder = os.path.join(RootFolder, '20140730')  # 30
-    #~ StartingFolder = os.path.join(RootFolder, '20140731')  # 262
-    #~ StartingFolder = os.path.join(RootFolder, '20140818')  # 20
-    #~ StartingFolder = os.path.join(RootFolder, '20140819')  # 64
-    #~ StartingFolder = os.path.join(RootFolder, '20140820')  # 64
-    #~ StartingFolder = os.path.join(RootFolder, '20140822')  # 149
-    #~ StartingFolder = os.path.join(RootFolder, '20140823')  # 6
-    #~ StartingFolder = os.path.join(RootFolder, '20140825')  # 99
-    #~ StartingFolder = os.path.join(RootFolder, '20140829')  # 4
-    #~ StartingFolder = os.path.join(RootFolder, '20140831')  # 309
-    #~ StartingFolder = os.path.join(RootFolder, '20140901')  # 149
-    #~ StartingFolder = os.path.join(RootFolder, '20140903')  # 30
-    #~ StartingFolder = os.path.join(RootFolder, '20140907')  # 277
-    #~ StartingFolder = os.path.join(RootFolder, '20140914')  # 47
+    StartingFolder = os.path.join(RootFolder, '20140721')  # 11
+    StartingFolder = os.path.join(RootFolder, '20140722')  # 44
+    StartingFolder = os.path.join(RootFolder, '20140724')  # 91
+    StartingFolder = os.path.join(RootFolder, '20140730')  # 30
+    StartingFolder = os.path.join(RootFolder, '20140731')  # 262
+    StartingFolder = os.path.join(RootFolder, '20140818')  # 20
+    StartingFolder = os.path.join(RootFolder, '20140819')  # 64
+    StartingFolder = os.path.join(RootFolder, '20140820')  # 64
+    StartingFolder = os.path.join(RootFolder, '20140822')  # 149
+    StartingFolder = os.path.join(RootFolder, '20140823')  # 6
+    StartingFolder = os.path.join(RootFolder, '20140825')  # 99
+    StartingFolder = os.path.join(RootFolder, '20140829')  # 4
+    StartingFolder = os.path.join(RootFolder, '20140831')  # 309
+    StartingFolder = os.path.join(RootFolder, '20140901')  # 149
+    StartingFolder = os.path.join(RootFolder, '20140903')  # 30
+    StartingFolder = os.path.join(RootFolder, '20140907')  # 277
+    StartingFolder = os.path.join(RootFolder, '20140914')  # 47
+    StartingFolder = os.path.join(RootFolder, '20140916')  # 51
+    StartingFolder = os.path.join(RootFolder, '20140920')  # 94
+    StartingFolder = os.path.join(RootFolder, '20140921')  # 227
     StartingFolder = RootFolder
 else:
     # If running on Ivans machine, look on the connected harddisk
@@ -87,6 +90,33 @@ print '\t- ', len(set(Modality)), 'modalities:'
 for i in set(Modality):
     print '\t\t-', i
 
+print
+print 'Values go from'
+print 'Exptime\t', min(Exposuretime), '-', max(Exposuretime)
+print 'SDD\t', min(SDD), '-', max(SDD)
+print 'Mean\t', min(Mean), '-', max(Mean)
+print 'Max\t', min(Max), '-', max(Max)
+print 'STD\t', min(STD), '-', max(STD)
+print
+
+plt.figure(figsize=[16,9])
+plt.subplot(151)
+plt.hist(Exposuretime,bins=128, normed=False, histtype='stepfilled')
+plt.title('Exposure times')
+plt.subplot(152)
+plt.hist(SDD,bins=128, normed=False, histtype='stepfilled')
+plt.title('Scintillator-detector distances')
+plt.subplot(153)
+plt.hist(Mean,bins=128, normed=False, histtype='stepfilled')
+plt.title('Image mean')
+plt.subplot(154)
+plt.hist(Max,bins=128, normed=False, histtype='stepfilled')
+plt.title('Image max')
+plt.subplot(155)
+plt.hist(STD,bins=128, normed=False, histtype='stepfilled')
+plt.title('Image standard deviation')
+plt.tight_layout()
+plt.draw()
 
 def subset(Selector, label=False):
     '''
@@ -103,19 +133,21 @@ def subset(Selector, label=False):
         flag]
     MaskedI = [str(item) for item, flag in zip(ExperimentID, Scintillator) if
         Selector in flag]
-    plot = ax.scatter(
+    currentaxis = fig.gca()
+    plot = currentaxis.scatter(
         MaskedX, MaskedY, MaskedZ,
         'o', c=MaskedC, cmap='hot', s=250)
     if label:
         for x, y, z, label in zip(MaskedX, MaskedY, MaskedZ, MaskedI):
-            ax.text(x, y, z, label)
-    ax.set_xlabel('Scintillator-CMOS distance [mm]')
-    ax.set_ylabel('Mean brightness of best image')
-    ax.set_zlabel('Standard deviation')
+            currentaxis.text(x, y, z, label)
 
-    ax.set_xlim([50, 300])
-    ax.set_ylim([0, 500])
-    ax.set_zlim([0, 100])
+    currentaxis.set_xlabel('Scintillator-CMOS distance [mm]')
+    currentaxis.set_ylabel('Mean brightness')
+    currentaxis.set_zlabel('Standard deviation')
+
+    currentaxis.set_xlim([50, 300])
+    currentaxis.set_ylim([0, 500])
+    currentaxis.set_zlim([0, 400])
 
     plt.title(' '.join([str(len(MaskedX)),
         'images for', Selector]))
@@ -125,28 +157,41 @@ def subset(Selector, label=False):
 fig = plt.figure(figsize=[16, 9])
 # Plot figure
 ## Setup plot
-plt.xkcd()
+#~ plt.xkcd()
 textalpha = 0.618
 
 ## Subplot Hamamatsu
-ax = fig.add_subplot(221, projection='3d')
-subset('Hamamatsu')
+ax1 = fig.add_subplot(221, projection='3d')
+subset('Hamamatsu', label=False)
 
 ## Subplot Pingseng
-ax = fig.add_subplot(222, projection='3d')
-subset('Pingseng')
+ax2 = fig.add_subplot(222, projection='3d')
+subset('Pingseng', label=False)
 
 ## Subplot Toshiba
-ax = fig.add_subplot(223, projection='3d')
-subset('Toshiba')
+ax3 = fig.add_subplot(223, projection='3d')
+subset('Toshiba', label=False)
 
 ## Subplot AppScinTech
-ax = fig.add_subplot(224, projection='3d')
-subset('AppScinTech')
+ax4 = fig.add_subplot(224, projection='3d')
+subset('AppScinTech', label=False)
 
 plt.tight_layout()
+plt.ion()
+plt.pause(0.0001)
+
+for ii in range(0, 361, 1):
+    ax1.azim = 60 + ii
+    ax2.azim = 60 + ii
+    ax3.azim = 60 + ii
+    ax4.azim = 60 + ii
+    sys.stdout.write("\rwriting angle %d/360" % ii)
+    plt.savefig('movie_' + str(ii).zfill(3) + '.png', transparent=True)
+    sys.stdout.flush()
+    plt.pause(0.0001)
+    plt.draw()
+sys.stdout.write("\n") # move the cursor to the next line
+
 OutputImage = os.path.join(StartingFolder, 'Overview.png')
 print 'Saving figure as', OutputImage
 plt.savefig(OutputImage, transparent=True)
-
-plt.show()
