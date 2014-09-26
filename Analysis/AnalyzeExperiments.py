51dbb7804cd18f189da34c737f490571a1780f54
David Haberthür <email@davidhaberthuer.ch>
2014-09-26 19:53:56 +0200

    Randomly select some images from the whole bunch
    We randomly select some image from the 2000 that Ivan made, sort them by
    either STD, mean or ID and present them in a figure.
    Like that we can hopefully weed out the unnecessary ones and get to the
    core of the result

--- Analysis/AnalyzeExperiments.py
+++ Analysis/AnalyzeExperiments.py
@@ -7,21 +7,13 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 from functions import myLogger
 from functions import get_git_hash
 
-# Where shall we start?
-if 'linux' in sys.platform:
-    # If running at the office, grep AFS
-    RootFolder = ('/afs/psi.ch/project/EssentialMed/MasterArbeitBFH/' +
+StartingFolder= ('/afs/psi.ch/project/EssentialMed/MasterArbeitBFH/' +
         'XrayImages')
-    StartingFolder = os.path.join(RootFolder, '20140921')  # 4
-    StartingFolder = RootFolder
-else:
-    # If running on Ivans machine, look on the connected harddisk
-    StartingFolder = ('/Volumes/WINDOWS/Aptina/Hamamatsu/AR0130/Computar-11A/')
-    StartingFolder = ('/Volumes/exFAT')
 
 # Go through all the results in the folder and show the images.
 Files = [os.path.join(dirpath, f)
@@ -29,10 +21,16 @@ Files = [os.path.join(dirpath, f)
     for f in files if f.endswith('.image.corrected.stretched.png')]
 print 'I found', len(Files), 'images to work with in', StartingFolder
 
-# Select every n'th image (http://stackoverflow.com/a/1404229/323100)
-Files = [Files[i] for i in xrange(0, len(Files), 156)]
+# Select some files
+randomizator = True
+if randomizator:
+    # Randomly select some files
+    Files = random.sample(Files, 15)
+else:
+    # Select every n'th image (http://stackoverflow.com/a/1404229/323100)
+    Files = [Files[i] for i in xrange(0, len(Files), 100)][:10]
 
-# Grab data from these files
+# Extract data from the images
 ExperimentID = [os.path.split(i)[1].split('.')[0] for i in Files]
 Images = [plt.imread(i) for i in Files]
 Mean = [np.mean(i) * 255 for i in Images]
@@ -48,7 +46,8 @@ for c, item in enumerate(Files):
     print '\tSTD:', round(STD[c], 1)
 
 # Sort according to something
-sort = 'std'
+sort = 'mean'
+
 if sort == 'std':
     sortedIDs = [x for (y, x) in sorted(zip(STD, ExperimentID),
         reverse=True)]
@@ -71,18 +70,28 @@ elif sort == 'id':
     sortedSTD = [x for (y, x) in sorted(zip(ExperimentID, STD),
         reverse=True)]
 
-plt.figure(figsize=[23, 8])
-bins = 255
+# full screen figure
+plt.figure(figsize=[25, 25])
+
 for c, i in enumerate(sortedImages):
-    sys.stdout.write(''.join(['\rImage %d/' % c, str(len(Images))]))
-    plt.subplot(2, len(Files), c + 1)
+    sys.stdout.write(''.join(['\rImage %d/' % (c + 1), str(len(Images))]))
+    if len(Files) > 10:
+        plt.subplot(int(np.ceil(len(Files) / 4)),
+            int(np.ceil(len(Files) / 3)), c + 1)
+    else:
+        plt.subplot(2, 5, c + 1)
     plt.imshow(i, cmap='bone')
-    plt.title(' '.join([sortedIDs[c], '\nMean', str(round(sortedMean[c], 1)),
-        '\nSTD', str(round(sortedSTD[c], 1))]))
+    currentDirName = os.path.dirname(Files[ExperimentID.index(sortedIDs[1])])[
+        len(os.path.dirname(os.path.commonprefix(Files))) + 1:]
+    plt.title(' '.join([sortedIDs[c], '| Mean', str(round(sortedMean[c], 1)), '| STD',
+        str(round(sortedSTD[c], 1))]))
+    plt.subplots_adjust(wspace=0.05)
+    plt.subplots_adjust(hspace=0.05)
     plt.axis('off')
-    plt.subplot(2, len(Files), c + len(Files) + 1)
-    plt.hist(i.flatten(), bins, histtype='stepfilled')
-    plt.ylim([0, 1.2e5])
+    plt.suptitle(' '.join(['sorted by', sort]))
+    #~ plt.subplot(2, len(Files), c + len(Files) + 1)
+    #~ plt.hist(i.flatten(), bins=256, histtype='stepfilled')
+    #~ plt.ylim([0, 1.2e5])
     sys.stdout.flush()
 
 print '\nShowing plot'
