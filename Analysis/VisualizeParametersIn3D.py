fc8d8c919a5ab04da2e815a88db137e385ca0963
David Haberthür <email@davidhaberthuer.ch>
2014-09-29 11:50:29 +0200

    Nicely animate plots (Progress Report 09/14)
    Using http://git.io/HnaW9A as help, we animate the plots, which makes
    them 'nice' for the progress report.
    This is much nicer than to just write out the movie frames as individual
    png files...

--- Analysis/VisualizeParametersIn3D.py
+++ Analysis/VisualizeParametersIn3D.py
@@ -13,6 +13,7 @@ import os
 import linecache
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import animation
 import numpy
 import sys
 
@@ -22,25 +23,25 @@ if 'linux' in sys.platform:
     RootFolder = ('/afs/psi.ch/project/EssentialMed/MasterArbeitBFH/' +
         'XrayImages')
     StartingFolder = os.path.join(RootFolder, '20140721')  # 11
-    StartingFolder = os.path.join(RootFolder, '20140722')  # 44
-    StartingFolder = os.path.join(RootFolder, '20140724')  # 91
-    StartingFolder = os.path.join(RootFolder, '20140730')  # 30
-    StartingFolder = os.path.join(RootFolder, '20140731')  # 262
-    StartingFolder = os.path.join(RootFolder, '20140818')  # 20
-    StartingFolder = os.path.join(RootFolder, '20140819')  # 64
-    StartingFolder = os.path.join(RootFolder, '20140820')  # 64
-    StartingFolder = os.path.join(RootFolder, '20140822')  # 149
-    StartingFolder = os.path.join(RootFolder, '20140823')  # 6
-    StartingFolder = os.path.join(RootFolder, '20140825')  # 99
-    StartingFolder = os.path.join(RootFolder, '20140829')  # 4
-    StartingFolder = os.path.join(RootFolder, '20140831')  # 309
-    StartingFolder = os.path.join(RootFolder, '20140901')  # 149
-    StartingFolder = os.path.join(RootFolder, '20140903')  # 30
-    StartingFolder = os.path.join(RootFolder, '20140907')  # 277
-    StartingFolder = os.path.join(RootFolder, '20140914')  # 47
-    StartingFolder = os.path.join(RootFolder, '20140916')  # 51
-    StartingFolder = os.path.join(RootFolder, '20140920')  # 94
-    StartingFolder = os.path.join(RootFolder, '20140921')  # 227
+    #~ StartingFolder = os.path.join(RootFolder, '20140722')  # 44
+    #~ StartingFolder = os.path.join(RootFolder, '20140724')  # 91
+    #~ StartingFolder = os.path.join(RootFolder, '20140730')  # 30
+    #~ StartingFolder = os.path.join(RootFolder, '20140731')  # 262
+    #~ StartingFolder = os.path.join(RootFolder, '20140818')  # 20
+    #~ StartingFolder = os.path.join(RootFolder, '20140819')  # 64
+    #~ StartingFolder = os.path.join(RootFolder, '20140820')  # 64
+    #~ StartingFolder = os.path.join(RootFolder, '20140822')  # 149
+    #~ StartingFolder = os.path.join(RootFolder, '20140823')  # 6
+    #~ StartingFolder = os.path.join(RootFolder, '20140825')  # 99
+    #~ StartingFolder = os.path.join(RootFolder, '20140829')  # 4
+    #~ StartingFolder = os.path.join(RootFolder, '20140831')  # 309
+    #~ StartingFolder = os.path.join(RootFolder, '20140901')  # 149
+    #~ StartingFolder = os.path.join(RootFolder, '20140903')  # 30
+    #~ StartingFolder = os.path.join(RootFolder, '20140907')  # 277
+    #~ StartingFolder = os.path.join(RootFolder, '20140914')  # 47
+    #~ StartingFolder = os.path.join(RootFolder, '20140916')  # 51
+    #~ StartingFolder = os.path.join(RootFolder, '20140920')  # 94
+    #~ StartingFolder = os.path.join(RootFolder, '20140921')  # 227
     StartingFolder = RootFolder
 else:
     # If running on Ivans machine, look on the connected harddisk
@@ -99,26 +100,29 @@ print 'Max\t', min(Max), '-', max(Max)
 print 'STD\t', min(STD), '-', max(STD)
 print
 
-plt.figure(figsize=[16,9])
-plt.subplot(151)
-plt.hist(Exposuretime,bins=128, normed=False, histtype='stepfilled')
-plt.title('Exposure times')
-plt.subplot(152)
-plt.hist(SDD,bins=128, normed=False, histtype='stepfilled')
-plt.title('Scintillator-detector distances')
-plt.subplot(153)
-plt.hist(Mean,bins=128, normed=False, histtype='stepfilled')
-plt.title('Image mean')
-plt.subplot(154)
-plt.hist(Max,bins=128, normed=False, histtype='stepfilled')
-plt.title('Image max')
-plt.subplot(155)
-plt.hist(STD,bins=128, normed=False, histtype='stepfilled')
-plt.title('Image standard deviation')
-plt.tight_layout()
-plt.draw()
+histograms = False
+if histograms:
+    plt.figure(figsize=[16, 9])
+    plt.subplot(151)
+    plt.hist(Exposuretime, bins=128, normed=False, histtype='stepfilled')
+    plt.title('Exposure times')
+    plt.subplot(152)
+    plt.hist(SDD, bins=128, normed=False, histtype='stepfilled')
+    plt.title('Scintillator-detector distances')
+    plt.subplot(153)
+    plt.hist(Mean, bins=128, normed=False, histtype='stepfilled')
+    plt.title('Image mean')
+    plt.subplot(154)
+    plt.hist(Max, bins=128, normed=False, histtype='stepfilled')
+    plt.title('Image max')
+    plt.subplot(155)
+    plt.hist(STD, bins=128, normed=False, histtype='stepfilled')
+    plt.title('Image standard deviation')
+    plt.tight_layout()
+    plt.draw()
 
-def subset(Selector, label=False):
+
+def subset_seletor(Selector, label=False):
     '''
     Select only a subset of items to present in the second plot, according to
     http://stackoverflow.com/a/3555387/323100
@@ -147,13 +151,13 @@ def subset(Selector, label=False):
 
     currentaxis.set_xlim([50, 300])
     currentaxis.set_ylim([0, 500])
-    currentaxis.set_zlim([0, 400])
+    currentaxis.set_zlim([0, 200])
 
     plt.title(' '.join([str(len(MaskedX)),
         'images for', Selector]))
     return Selector
 
-## Do the plot
+## Prepare the plot
 fig = plt.figure(figsize=[16, 9])
 # Plot figure
 ## Setup plot
@@ -162,40 +166,58 @@ textalpha = 0.618
 
 ## Subplot Hamamatsu
 ax1 = fig.add_subplot(221, projection='3d')
-subset('Hamamatsu', label=False)
+subset_seletor('Hamamatsu', label=False)
 
 ## Subplot Pingseng
 ax2 = fig.add_subplot(222, projection='3d')
-subset('Pingseng', label=False)
+subset_seletor('Pingseng', label=False)
 
 ## Subplot Toshiba
 ax3 = fig.add_subplot(223, projection='3d')
-subset('Toshiba', label=False)
+subset_seletor('Toshiba', label=False)
 
 ## Subplot AppScinTech
 ax4 = fig.add_subplot(224, projection='3d')
-subset('AppScinTech', label=False)
+subset_seletor('AppScinTech', label=False)
 
 plt.tight_layout()
 
-animate = True
+Animate = True
+if Animate:
+    # Initialization function: Initialize plot, move the camera
+    def init():
+        Azimuth = 66
+        Elevation = 15
+        for axis in (ax1, ax2, ax3, ax4):
+            axis.azim = Azimuth
+            axis.elev = Elevation
+        return ax1, ax2, ax3, ax4,
 
-if animate:
-    plt.ion()
-    plt.pause(0.0001)
-    for ii in range(0, 361, 1):
-        ax1.azim = 60 + ii
-        ax2.azim = 60 + ii
-        ax3.azim = 60 + ii
-        ax4.azim = 60 + ii
-        sys.stdout.write("\rwriting angle %d/360" % ii)
-        plt.savefig('movie_' + str(ii).zfill(3) + '.png', transparent=False)
-        sys.stdout.flush()
-        plt.pause(0.0001)
-        plt.draw()
-    sys.stdout.write("\n") # move the cursor to the next line
+    # Animation function.
+    def animate_camera(i):
+        for axis in (ax1, ax2, ax3, ax4):
+            axis.azim = 45 + 40 * numpy.sin(numpy.radians(i))
+            axis.elev = 30 + 10 * numpy.sin(numpy.radians(i))
+        print 'animating frame', i, 'of', numframes
+        return ax1, ax2, ax3, ax4,
+
+    # Call the actual animation function.
+    numframes = 360
+    Movie = animation.FuncAnimation(fig, animate_camera, init_func=init,
+                                   frames = numframes, interval=10, blit=True)
+
+    # Save the animation as an mp4.
+    Movie.save('scintillators.mp4', fps=24, extra_args=['-vcodec', 'libx264'])
+
+    plt.show()
 else:
-    OutputImage = os.path.join(StartingFolder, 'Overview.png')
-    print 'Saving figure as', OutputImage
-    plt.savefig(OutputImage, transparent=False)
+    # Choose view
+    # Brightness-Distance 0/90
+    # STD-Distance 90/0
+    # STD-Brightness 0/0
+    Azimuth = 0
+    Elevation = 0
+    for axis in (ax1, ax2, ax3, ax4):
+        axis.azim = Azimuth
+        axis.elev = Elevation
     plt.show()
