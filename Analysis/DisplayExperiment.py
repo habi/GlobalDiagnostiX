81cea8bcb70b6bd2ee70383e72e692376fd51d02
David Haberthür <email@davidhaberthuer.ch>
2014-10-03 10:08:14 +0200

    Initial commit of script to display experiments
    While going through the resolution analysis we detect good and bad
    experiment IDs.
    This script should help us to quickly go through the good ones and
    display their images.

---
+++ Analysis/DisplayExperiment.py
@@ -0,0 +1,24 @@
+"""
+Script to display only some experiments, based on either choice of ID or choice
+of component
+"""
+
+
+ID = [84466438,
+    8466246,
+    8596690,
+    8595968,
+    5808743,
+    5814382,
+    5814190,
+    9078293,
+    9074884,
+    5467590,
+    9071872,
+    8466170,
+]
+
+for i in ID:
+    print i
+
+print 'Fertig'
