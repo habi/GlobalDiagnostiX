0fab7d61c8ba81455c8083350857d567ea20f68b
David Haberthür <email@davidhaberthuer.ch>
2014-11-20 18:09:08 +0100

    Add zooms and display everything a bit nicer

--- Demonstrator/DisplayExperiment.py
+++ Demonstrator/DisplayExperiment.py
@@ -28,11 +28,15 @@ CorrectedMean = numpy.zeros(len(Radiographies))
 CorrectedMax = numpy.zeros(len(Radiographies))
 CorrectedSTD = numpy.zeros(len(Radiographies))
 
+ZoomedMean = numpy.zeros(len(Radiographies))
+ZoomedMax = numpy.zeros(len(Radiographies))
+ZoomedSTD = numpy.zeros(len(Radiographies))
+
 # Display difference
 plt.figure(figsize=(16, 9))
 for counter in range(len(Radiographies)):
     # Inform user
-    print counter, 'of', len(Radiographies), 'Reading Images'
+    print counter + 1, 'of', len(Radiographies), 'Reading Images'
 
     # Grab data
     ImageData = numpy.fromfile(Radiographies[counter],
@@ -41,12 +45,20 @@ for counter in range(len(Radiographies)):
     DarkData = numpy.fromfile(Darks[counter], dtype=numpy.int16).reshape(
         CameraHeight, CameraWidth)
     CorrectedData = ImageData - DarkData
+    ZoomedData = CorrectedData[445:775, 510:615]
+
     Mean[counter] = numpy.mean(ImageData)
     Max[counter] = numpy.max(ImageData)
     STD[counter] = numpy.std(ImageData)
-    CorrectedMean[counter] = numpy.mean(ImageData)
-    CorrectedMax[counter] = numpy.max(ImageData)
-    CorrectedSTD[counter] = numpy.std(ImageData)
+
+    CorrectedMean[counter] = numpy.mean(CorrectedData)
+    CorrectedMax[counter] = numpy.max(CorrectedData)
+    CorrectedSTD[counter] = numpy.std(CorrectedData)
+
+    ZoomedMean[counter] = numpy.mean(ZoomedData)
+    ZoomedMax[counter] = numpy.max(ZoomedData)
+    ZoomedSTD[counter] = numpy.std(ZoomedData)
+
     ExposureTime = os.path.basename(Radiographies[counter]).split('-e')[
         1].split('-g')[0]
     Gain = os.path.basename(Radiographies[counter]).split('-g')[1].split(
@@ -54,36 +66,58 @@ for counter in range(len(Radiographies)):
 
     # Display data
     plt.subplot(4, len(Radiographies), counter + 1)
-    plt.imshow(ImageData, cmap='bone', vmin=numpy.mean(ImageData),
-        vmax=numpy.max(ImageData))
+    plt.imshow(ImageData, cmap='bone', vmin=0, vmax=2 ** 10,
+               interpolation='bicubic')
     plt.axis('off')
-    plt.title('\n'.join([ExposureTime + ' ms', 'Gain ' + Gain]))
+    plt.title('Gain ' + Gain)
+    plt.subplots_adjust(hspace=0.01)
+    plt.subplots_adjust(wspace=0.01)
 
     plt.subplot(4, len(Radiographies), counter + 1 + len(Radiographies))
-    #~ plt.hist(ImageData.flatten(), bins=128, fc='k', ec='k')
+    plt.imshow(CorrectedData, cmap='bone', interpolation='bicubic',
+               vmax=CorrectedMean[counter] + 3 * CorrectedSTD[counter])
     plt.axis('off')
-
-    #~ plt.subplot(5, len(Radiographies), counter + 1 + 2 * len(Radiographies))
-    plt.imshow(CorrectedData, cmap='bone', vmin=0, vmax=100)
-    plt.axis('off')
-    plt.title('Corrected Image')
+    plt.title('Mean+3xSTD')
+    plt.subplots_adjust(hspace=0.025)
+    plt.subplots_adjust(wspace=0.025)
 
     plt.subplot(4, len(Radiographies), counter + 1 + 2 * len(Radiographies))
-    plt.hist(CorrectedData.flatten(), bins=128, fc='k', ec='k')
-    plt.yticks=[1,2]
+    plt.imshow(ZoomedData, cmap='bone', interpolation = 'bicubic',
+               vmax=CorrectedMean[counter] + 3 * CorrectedSTD[counter])
+    plt.axis('off')
+    plt.title('Zoom')
+    plt.subplots_adjust(hspace=0.025)
+    plt.subplots_adjust(wspace=0.025)
 
-    plt.subplots_adjust(hspace = 0.5)
+    ShowHistograms = False
+    if ShowHistograms:
+        plt.hist(ImageData.flatten(), log=True, bins=32, fc='y', ec='y',
+            alpha=0.25)
+        plt.hist(CorrectedData.flatten(), bins=32, fc='k', ec='k', alpha=0.5)
+        # turn off tick labels of histogram
+        plt.gca().get_xaxis().set_ticks([])
+        plt.gca().get_yaxis().set_ticks([])
+
 
 # Plot max, mean and standard deviation of images
 plt.subplot(414)
-plt.plot(Max,'-o',label='Max')
-plt.plot(Mean,'-o',label='Mean')
-plt.plot(STD,'-o',label='STD')
+plt.plot(Max, '-', label='Max')
+plt.plot(Mean, '-', label='Mean')
+plt.plot(STD, '-', label='STD')
 
-plt.plot(CorrectedMax,'-o',label='Corrected Max')
-plt.plot(CorrectedMean,'-o',label='Corrected Mean')
-plt.plot(CorrectedSTD,'-o',label='Corrected STD')
+plt.plot(CorrectedMax, '-o', label='Corrected Max')
+plt.plot(CorrectedMean, '-o', label='Corrected Mean')
+plt.plot(CorrectedSTD, '-o', label='Corrected STD')
+
+#~ plt.plot(ZoomedMax, '-*', label='Zoomed Max')
+#~ plt.plot(ZoomedMean, '-*', label='Zoomed Mean')
+#~ plt.plot(ZoomedSTD, '-*', label='Zoomed STD')
+
+plt.xlim([-0.5, len(Radiographies) - 0.5])
+plt.xlabel('Image')
+plt.ylabel('Brightness')
 
 plt.legend(loc='best')
-plt.tight_layout()
+
+plt.savefig('Gainseries.png')
 plt.show()
