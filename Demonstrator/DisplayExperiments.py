a68bdc09e7b7e436969987fb4bec2e51780e0bf3
David Haberthür <email@davidhaberthuer.ch>
2014-11-24 17:37:37 +0100

    Also display histogram of each image
    And some general cleanup of the script

--- Demonstrator/DisplayExperiments.py
+++ Demonstrator/DisplayExperiments.py
@@ -3,13 +3,13 @@ Script to read and display the experiments done with the iAi electronics
 prototype in the x-ray lab
 """
 
-import glob
 import os
+import glob
 import numpy
 import matplotlib.pylab as plt
 import platform
 
-# CameraSize
+# Setup
 CameraWidth = 1280
 CameraHeight = 1024
 
@@ -20,11 +20,11 @@ else:
     RootPath = '/afs/psi.ch/project/EssentialMed/Images' \
                '/DetectorElectronicsTests'
 
-# Get Images from all folders in RootPath. But only get the ones from IP 44,
-# since that was the one that was focus and aligned properly for this test.
-FolderList = sorted(glob.glob(os.path.join(RootPath, '*')))
+# Get all subfolders: http://stackoverflow.com/a/973488/323100
+FolderList = os.walk(RootPath).next()[1]
 
-# Grab names
+# Get images from the module with IP 44, since that was the one that was focus
+# and aligned properly for this test
 RadiographyName = [glob.glob(os.path.join(RootPath, i, '*1-44.gray'))[0] for
     i in FolderList]
 DarkName = [glob.glob(os.path.join(RootPath, i, '*0-44.gray'))[0] for
@@ -40,6 +40,10 @@ Dark = [numpy.fromfile(i, dtype=numpy.int16).reshape(CameraHeight, CameraWidth)
 print 'Calculating corrected images'
 CorrectedData = [Radiography[i] - Dark[i] for i in range(len(FolderList))]
 CorrectedAdat = [Dark[i] - Radiography[i] for i in range(len(FolderList))]
+
+#~ # Shift gray values of corrected data to min=0
+#~ CorrectedData = [ i - numpy.min(i) for i in CorrectedData]
+#~ CorrectedAdat = [ i - numpy.min(i) for i in CorrectedAdat]
 
 # Grab parameters from filename
 kV = [os.path.basename(i).split('kV_')[0].split('_')[-1] for i in FolderList]
@@ -71,47 +75,90 @@ for counter, Folder in enumerate(FolderList):
     print '\nFor the experiment with', kV[counter], 'kV,', mAs[counter], \
         'mAs we have the following image properties'
     print '\tMin\tMean\tMax\tSTD'
-    print 'Image\t',    round(ValuesImage[counter][0], 1), '\t', \
+    print 'Image\t', round(ValuesImage[counter][0], 1), '\t', \
         round(ValuesImage[counter][1], 1), '\t', \
         round(ValuesImage[counter][2], 1), '\t', \
         round(ValuesImage[counter][3], 1)
-    print 'Dark\t',    round(ValuesDark[counter][0], 1), '\t', \
+    print 'Dark\t', round(ValuesDark[counter][0], 1), '\t', \
         round(ValuesDark[counter][1], 1), '\t', \
         round(ValuesDark[counter][2], 1), '\t', \
         round(ValuesDark[counter][3], 1)
-    print 'Img-Drk\t',    round(ValuesCorrectedData[counter][0], 1), '\t', \
+    print 'Img-Drk\t', round(ValuesCorrectedData[counter][0], 1), '\t', \
         round(ValuesCorrectedData[counter][1], 1), '\t', \
         round(ValuesCorrectedData[counter][2], 1), '\t', \
         round(ValuesCorrectedData[counter][3], 1)
-    print 'Drk-Imgk\t',    round(ValuesCorrectedAdat[counter][0], 1), '\t', \
+    print 'Drk-Img\t', round(ValuesCorrectedAdat[counter][0], 1), '\t', \
         round(ValuesCorrectedAdat[counter][1], 1), '\t', \
         round(ValuesCorrectedAdat[counter][2], 1), '\t', \
         round(ValuesCorrectedAdat[counter][3], 1)
 
-    plt.figure(figsize=(16, 9))
+    plt.figure(figsize=(20, 10))
     FigureTitle = str(counter + 1) + '/' + str(len(FolderList)), '|', \
-        os.path.basename(Folder), '\nXray shot with', kV[counter], 'kV and',\
+        os.path.basename(Folder), ' Xray shot with', kV[counter], 'kV and',\
         mAs[counter], 'mAs (' + SourceExposureTime[counter] + \
-        'ms source exposure time).\nCaptured with', \
+        'ms source exposure time). Captured with', \
         CMOSExposureTime[counter], 'ms CMOS exposure time and Gain', \
         Gain[counter]
     plt.suptitle(' '.join(FigureTitle))
-    plt.subplot(221)
-    plt.imshow(Radiography[counter], cmap='bone', interpolation='nearest')
+
+    plt.subplot(241)
+    plt.imshow(Radiography[counter], cmap='bone', interpolation='bicubic',
+        vmin=ValuesImage[counter][0],
+        vmax=ValuesImage[counter][1] + 3 * ValuesImage[counter][3])
     plt.title('Image')
-    plt.subplot(222)
-    plt.imshow(Dark[counter], cmap='bone', interpolation='nearest')
+    plt.axis('off')
+
+    plt.subplot(242)
+    plt.hist(Radiography[counter].flatten(), bins=128, fc='k', ec='k')
+    plt.axvline(x=ValuesImage[counter][0], color='r', linestyle='--')
+    plt.axvline(x=ValuesImage[counter][1] + 3 * ValuesImage[counter][3],
+        color='r', linestyle='--')
+    plt.title('Image Histogram (red=display range)')
+
+    plt.subplot(243)
+    plt.imshow(Dark[counter], cmap='bone', interpolation='bicubic',
+        vmin=ValuesDark[counter][0],
+        vmax=ValuesDark[counter][1] + 3 * ValuesDark[counter][3])
     plt.title('Dark')
-    plt.subplot(223)
-    plt.imshow(CorrectedData[counter], cmap='bone', interpolation='nearest',
+    plt.axis('off')
+
+    plt.subplot(244)
+    plt.hist(Dark[counter].flatten(), bins=128, fc='k', ec='k')
+    plt.axvline(x=ValuesDark[counter][0], color='r', linestyle='--')
+    plt.axvline(x=ValuesDark[counter][1] + 3 * ValuesDark[counter][3],
+        color='r', linestyle='--')
+    plt.title('Dark Histogram (red=display range)')
+
+    plt.subplot(245)
+    plt.imshow(CorrectedData[counter], cmap='bone', interpolation='bicubic',
         vmin=ValuesCorrectedData[counter][0],
         vmax=(ValuesCorrectedData[counter][1] + 3 *
             ValuesCorrectedData[counter][3]))
     plt.title('Img-Drk')
-    plt.subplot(224)
-    plt.imshow(CorrectedAdat[counter], cmap='bone', interpolation='nearest',
+    plt.axis('off')
+
+    plt.subplot(246)
+    plt.hist(CorrectedData[counter].flatten(), bins=128, fc='k', ec='k')
+    plt.axvline(x=ValuesCorrectedData[counter][0], color='r', linestyle='--')
+    plt.axvline(x=ValuesCorrectedData[counter][1] +
+        3 * ValuesCorrectedData[counter][3], color='r', linestyle='--')
+    plt.title('Corrected Histogram (red=display range)')
+
+    plt.subplot(247)
+    plt.imshow(CorrectedAdat[counter], cmap='bone', interpolation='bicubic',
         vmin=ValuesCorrectedAdat[counter][0],
-        vmax=(ValuesCorrectedAdat[counter][1] + 3 *
-            ValuesCorrectedAdat[counter][3]))
+        vmax=(ValuesCorrectedAdat[counter][1] +
+            3 * ValuesCorrectedAdat[counter][3]))
     plt.title('Drk-Img')
+    plt.axis('off')
+
+    plt.subplot(248)
+    plt.hist(CorrectedAdat[counter].flatten(), bins=128, fc='k', ec='k')
+    plt.axvline(x=ValuesCorrectedAdat[counter][0], color='r', linestyle='--')
+    plt.axvline(x=ValuesCorrectedAdat[counter][1] +
+        3 * ValuesCorrectedAdat[counter][3], color='r', linestyle='--')
+    plt.title('Corrected Histogram (red=display range)')
+
+    plt.savefig(Folder + '.png')
+
     plt.show()
