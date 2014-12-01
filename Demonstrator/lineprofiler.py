be8096be4dac5cac367209de72874c45550d8b09
David Haberthür <email@davidhaberthuer.ch>
2014-12-01 16:34:21 +0100

    Also allow for input of coordinates
    If the user inputs coordinates, we plot those.
    If the user does not input coordinates, we let him/her select them
    The function returns coordinates and the line profile of the image along
    the coordinates.

--- Demonstrator/lineprofiler.py
+++ Demonstrator/lineprofiler.py
@@ -1,9 +1,15 @@
-def lineprofile(inputimage, debug=False):
+def lineprofile(inputimage, coordinates=False, debug=False):
     """
     Function to draw a line profile from a selection in the image.
     Comes in handy to look at the resolution phantom in x-ray images shot with
     the iAi electronics prototype in the x-ray lab.
     Based on http://stackoverflow.com/a/7880726/323100
+    If the user supplies coordinates (in the form of coordinates =
+    ((x0,y0), (x1,y1)), we use those, otherwise we let the user select some.
+    If "debug" is set to true, we show Lena.
+
+	The function returns the coordinates and the lineprofile of the input image
+	along these coordinates.
     """
 
     # suppress pep8 warning about potentially unreferenced variables
@@ -12,27 +18,35 @@ def lineprofile(inputimage, debug=False):
     import numpy
     import scipy.ndimage
     import matplotlib.pyplot as plt
+    import random
 
     # Debug
     if debug:
         inputimage = scipy.misc.lena()
 
     # Prepare image
-    plt.figure(0, figsize=(16, 16))
+    plt.figure(random.randint(0,1000), figsize=(16, 16))
     if debug:
         plt.ion()
     plt.imshow(inputimage, cmap='bone', vmin=numpy.min(inputimage),
                vmax=numpy.mean(inputimage) + 3 * numpy.std(inputimage))
     plt.title('Please select the end-points for the line-profile')
 
-    # Let the user select a line to plot the profile from
-    pts = []
-    while len(pts) < 2:
-        pts = numpy.asarray(plt.ginput(2, timeout=-1))
-        x0 = pts[1, 0]
-        x1 = pts[0, 0]
-        y0 = pts[1, 1]
-        y1 = pts[0, 1]
+    if coordinates:
+        # The user gave coordinates to use. Use them.
+        x0 = coordinates[0][0]
+        y0 = coordinates[0][1]
+        x1 = coordinates[1][0]
+        y1 = coordinates[1][1]
+    else:
+        # Let the user select a line to plot the profile from
+        pts = []
+        while len(pts) < 2:
+            pts = numpy.asarray(plt.ginput(2, timeout=-1))
+            x0 = pts[1, 0]
+            x1 = pts[0, 0]
+            y0 = pts[1, 1]
+            y1 = pts[0, 1]
 
     # Interpolate a line between x0, y0 and x1, y1 with double the length of
     # the line
@@ -60,11 +74,12 @@ def lineprofile(inputimage, debug=False):
     plt.plot(profileinterpolated, 'red',  label='interpolated')
     plt.plot(profilenn, 'orange', label='nearest neighbour')
     plt.plot(0, profilenn[0], 'yo', markersize=25, alpha=0.309)
-    plt.plot(len(profilenn)-1, profilenn[-1], 'ko', markersize=25, alpha=0.309)
-    plt.xlim([0, len(profilenn)-1])
+    plt.plot(len(profilenn) - 1, profilenn[-1], 'ko', markersize=25,
+             alpha=0.309)
+    plt.xlim([0, len(profilenn) - 1])
     plt.legend(loc='best')
     plt.draw()
     plt.ioff()
     if debug:
         plt.show()
-    return pts, profileinterpolated
+    return ((x0,y0),(x1,y1)), profileinterpolated
