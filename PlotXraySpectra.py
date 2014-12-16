118c4cf16f30988c1ccbe0e230e6d866a0b9812e
David Haberthür <email@davidhaberthuer.ch>
2014-12-16 17:58:04 +0100

    Increase landscape.io code health
    And make display more nice

--- PlotXraySpectra.py
+++ PlotXraySpectra.py
@@ -21,12 +21,13 @@ Spectra = [
 Data = [(np.loadtxt(FileName)) for FileName in Spectra]
 Energy = [int(open(FileName).readlines()[2].split()[4])
           for FileName in Spectra]
-Mean = [np.round(double(open(FileName).readlines()[5].split()[3]),
-                 decimals=2) for FileName in Spectra]
+Mean = [float(open(FileName).readlines()[5].split()[3])
+        for FileName in Spectra]
 
 for i in range(len(Spectra)):
     plt.plot(Data[i][:, 0], Data[i][:, 1],
-             label=str(Energy[i]) + 'kV, Mean=' + str(Mean[i]) + 'keV')
+             label=str(Energy[i]) + 'kV, Mean=' +
+                   str(round(Mean[i], 3)) + 'keV')
 
 plt.legend(loc='best')
 plt.title('X-ray spectra')
@@ -34,13 +35,19 @@ plt.xlabel('Energy [kV]')
 plt.ylabel('Photons')
 plt.savefig('plot.pdf')
 
-for WhichOneShallWeIntegrate in range(len(Spectra)):
-    #~ plt.figure()
-    #~ plt.plot(Data[WhichOneShallWeIntegrate][:, 0],
-            #~ Data[WhichOneShallWeIntegrate][:, 1])
-
-    Integral = scipy.integrate.trapz(Data[WhichOneShallWeIntegrate][:, 1],
-                                    Data[WhichOneShallWeIntegrate][:, 0])
-    print 'The integral for', Energy[WhichOneShallWeIntegrate], 'kV is',\
+plt.figure(figsize=[22, 5])
+for counter, spectra in enumerate(Spectra):
+    plt.subplot(1, len(Spectra), counter + 1)
+    plt.plot(Data[counter][:, 0], Data[counter][:, 1])
+    Integral = scipy.integrate.trapz(Data[counter][:, 1], Data[counter][:, 0])
+    print 'The integral for', Energy[counter], 'kV is', \
         str(round(Integral / 1e6, 3)) + 'e6 photons'
-    #~ plt.show()
+    plt.title(str(Energy[counter]) + 'kV\n' +
+              str(round(Integral / 1e6, 3)) + 'e6 photons')
+    plt.xlim([0, 150])
+    plt.ylim([0, 3e6])
+    # Turn off y-ticks for subplots 2-end (counter >0)
+    if counter:
+        plt.setp(plt.gca().get_yticklabels(), visible=False)
+plt.tight_layout()
+plt.show()
