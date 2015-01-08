e23be3714266d7a508fc30c75a88994eb6dde607
David Haberthür <email@davidhaberthuer.ch>
2015-01-08 17:41:32 +0100

    Adapt from original to *my* coding style
    Changing variables to be more descriptive and adding some comments

--- Demonstrator/undistort_images.py
+++ Demonstrator/undistort_images.py
@@ -17,66 +17,73 @@ import matplotlib.pylab as plt
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
 
-# Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# We *have* to give the pattern size to look for. This is the number of chess
+# board fields. (7,6) seems to be good -> Number of rows/colums -1.
+PatternSize = (7, 6)
+# Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ...., (6,5,0)
+ObjectPoints = np.zeros((PatternSize[1] * PatternSize[0], 3), np.float32)
+ObjectPoints[:, :2] = np.mgrid[0:PatternSize[0],
+                               0:PatternSize[1]].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 # 3d point in real world space
-objpoints = []
+RealWorldPoints = []
 # 2d points in image plane.
-imgpoints = []
+ImagePoints = []
 
 images = glob.glob('left*.jpg')
 
 if not len(images):
     print 'Download left*.jpg from http://git.io/MDUBRw and save these', \
         'images in the same folder as this script'
+    exit('FilesNotFound')
 
-plt.figure(figsize=[16,9])
-for counter, fname in enumerate(images):
-    print 'processing %s...' % fname
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+plt.figure('Original images', figsize=[16, 9])
+for counter, FileName in enumerate(images):
+    print 'processing %s...' % FileName
+    Image = cv2.imread(FileName)
+    Image_BW = cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners. We *have* to give the pattern size to look
-    # for.
-    patternsize = (7,6)
-    found, corners = cv2.findChessboardCorners(gray, patternsize)
+    # Find the chess board corners.
+    Found, Corners = cv2.findChessboardCorners(Image_BW, PatternSize)
 
-    # If found, add object points, image points (after refining them)
-    if found:
-        # find more precise points
-        cv2.cornerSubPix(gray,corners,(5,5),(-1,-1),criteria)
-
-        imgpoints.append(corners.reshape(-1,2))
-        objpoints.append(objp)
-
-        # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners, found)
-    plt.subplot(3,5,counter+1)
-    plt.imshow(img)
-    plt.title(fname)
+    # If found, refine the image points, add them to their lists and draw
+    # the chessboard corners on the images
+    if Found:
+        # Find more precise points. The fist tuple influences the side length
+        # of the search window. The second tuple is the dead region in the
+        # middle of the search zone, see http://is.gd/xm6SXi
+        cv2.cornerSubPix(Image_BW, Corners, (25, 25), (-1, -1), criteria)
+        ImagePoints.append(Corners)
+        RealWorldPoints.append(ObjectPoints)
+        cv2.drawChessboardCorners(Image, PatternSize, Corners, Found)
+    plt.subplot(3, 5, counter + 1)
+    plt.imshow(Image)
+    plt.title(FileName)
     plt.axis('off')
-plt.show()
 
-rms, camera_matrix, dist_coefs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape, None, None)
-print "RMS:", rms
-print "camera matrix:\n", camera_matrix
-print "distortion coefficients: ", dist_coefs.ravel()
+RMS, CameraMatrix, DistortionCoefficients, rvecs, tvecs = \
+    cv2.calibrateCamera(RealWorldPoints, ImagePoints, Image_BW.shape, None,
+                        None)
+print "RMS:", RMS
+print "camera matrix:\n", CameraMatrix
+print "distortion coefficients: ", DistortionCoefficients.ravel()
 
-plt.figure(figsize=[16,9])
-for counter, fname in enumerate(images):
-    print 'undistorting %s...' % fname
-    img = cv2.imread(fname)
-    h,  w = img.shape[:2]
-    newcameramtx, roi=cv2.getOptimalNewCameraMatrix(camera_matrix,dist_coefs,(w,h),1,(w,h))
+plt.figure('Undistorted images', figsize=[16, 9])
+for counter, FileName in enumerate(images):
+    print 'undistorting %s...' % FileName
+    Image = cv2.imread(FileName)
+    NewCameraMatrix, ROI = \
+        cv2.getOptimalNewCameraMatrix(CameraMatrix, DistortionCoefficients,
+                                      (Image.shape[1], Image.shape[0]), 1,
+                                      (Image.shape[1], Image.shape[0]))
 
-    # undistort
-    dst = cv2.undistort(img, camera_matrix, dist_coefs, None, newcameramtx)
+    # Undistort images
+    UndistorImage = cv2.undistort(Image, CameraMatrix, DistortionCoefficients,
+                                  None, NewCameraMatrix)
 
-    plt.subplot(3,5,counter+1)
-    plt.imshow(dst)
-    plt.title(fname)
+    plt.subplot(3, 5, counter + 1)
+    plt.imshow(UndistorImage)
+    plt.title(FileName)
     plt.axis('off')
 plt.show()
